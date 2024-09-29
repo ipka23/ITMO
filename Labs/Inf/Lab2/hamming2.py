76815361af7e09002710b45a8db640f09148d03a
ipka23 <ilyaipka@gmail.com>
2024-09-29 18:11:03 +0300

    dd

--- Labs/Inf/Lab2/hamming2.py
+++ Labs/Inf/Lab2/hamming2.py
@@ -1,4 +1,4 @@
-message = '1111000'
+message = input()
 
 def xor(x1, x2, x3, x4):
     return str(sum(map(int, x1 + x2 + x3 + x4)) % 2)
@@ -11,19 +11,16 @@ def S(s1, s2, s3):
 s1 = xor(message[0], message[2], message[4], message[6])
 s2 = xor(message[1], message[2], message[5], message[6])
 s3 = xor(message[3], message[4], message[5], message[6])
-print(int(s3 + s2 + s1, 2))
 
-for i in range(4,5):
-    print(message[:i-1:] + '0' + message[i:])
+
+for i in range(0,8):
     if S(s1, s2, s3) == 0 and i == 0:
         print(message)
     if S(s1, s2, s3) == i and message[i-1] == '1':
-        message = message[0:i-2] + '0' + message[i+1:]
+        message = message[:i-1] + '0' + message[i:]
+        print(message)
+        break
     if S(s1, s2, s3) == i and message[i-1] == '0':
-        message = message[0:i-1] + '1' + message[i+1:]
-        print(message, i, S(s1, s2, s3), message[i-1])
-
-
-# m = '000'
-# m = m[0:2] + '1'
-# print(m)
+        message = message[:i-1] + '1' + message[i:]
+        print(message)
+        break
