e3b565883872a86392a5b83884e997b484b258a4
ipka23 <ilyaipka@gmail.com>
2024-10-01 23:59:55 +0300

    dd

---
+++ Labs/OPD/Lab1/1.sh
@@ -0,0 +1,24 @@
+mkdir lab0
+cd lab0 
+mkdir -p clamper4/vileplume clamper4/poochyena
+cd clamper4
+echo "satk=5 sdef=9 spd=8" > stoutland
+cd ..
+echo "Возможности Overland=4\nSurface=2 Jump=2 Power=1 Intelligence=5\nTelekinetic=0" > duosion6 
+echo "weigth=89.1 height=28.0 atk=14\ndef=6" > excadrill7 
+mkdir graveler2
+cd graveler2
+echo "weigth=209.4 height=67.0 atk=10\ndef=7" > rapidash 
+echo "satk=5 sdef=5 spd=10" > staraptor
+echo "Развитые способности\nContrary" > servine 
+cd .. 
+mkdir taillow0
+cd taillow0
+echo "Живет Beach Freshwater Ocean" > blastoise 
+echo "Тип диеты\nNullivore" > bronzong 
+echo "satk=7 sdef=8 spd=5" > beartic 
+echo "Возможности\nOverland=3 Surface=2 Sky=8 Jump=2 Power1=0 Intelligence=4 Telepath=0" > chimecho 
+echo "Способности Growl Absorb Nature Power Fake Out\nFury Swipes Water Sport Bubblebeam Zen Headbutt Uproar Hydro\nPump" > lombre
+cd ..
+echo "Возможности Overland=5 Surface=4 Jump=1 Power=2\nIntelligence=4 Inflatable=0" > trubbish4
+
