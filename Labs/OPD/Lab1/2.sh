099705a1a1e755fb44a25b90779ae0393091e4b4
ipka23 <ilyaipka@gmail.com>
2024-10-04 21:42:29 +0300

    dd

---
+++ Labs/OPD/Lab1/2.sh
@@ -0,0 +1,43 @@
+chmod u+rwx-w clamper4
+chmod g+rwx clamper4 
+chmod o+rwx clamper4
+cd clamper4
+chmod u+rw-x stoutland
+chmod g+w-r-x stoutland
+chmod o+w-r-x stoutland
+chmod a= vileplume
+chmod u+wx vileplume
+chmod g+rwx vileplume
+chmod o+rx vileplume
+chmod a= poochyena
+chmod u+wx poochyena
+chmod g+rw poochyena
+chmod o+wx poochyena
+cd ..
+chmod 404 duosion6
+chmod a= excadrill7
+chmod a+r excadrill7
+chmod a= graveler2
+chmod u+rwx graveler2
+chmod g+wx graveler2
+chmod o+rwx graveler2
+cd graveler2
+chmod u+rw-x rapidash
+chmod g= rapidash
+chmod o+r-wx rapidash
+chmod a= staraptor
+chmod g+r staraptor
+chmod o+r staraptor
+chmod 046 servine
+cd ..
+chmod 537 taillow0
+cd taillow0
+chmod 006 blastoise
+chmod 400 bronzong
+chmod 440 beartic
+chmod u= chimecho
+chmod g= chimecho
+chmod o+rw chimecho
+chmod 640 lombre
+cd ..
+chmod 440 trubbish4
