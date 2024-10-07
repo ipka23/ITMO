0c92ce710681a71c8a98d03da76f7e97cdad0f0c
ipka23 <ilyaipka@gmail.com>
2024-10-07 21:07:36 +0300

    dd

--- Labs/Prog/Lab2/Lab2/src/Main.java
+++ Labs/Prog/Lab2/Lab2/src/Main.java
@@ -1,7 +1,15 @@
+import pokemons.Honedge;
+import ru.ifmo.se.pokemon.Battle;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        Battle b = new Battle();
+        Honedge p1 = new Honedge("Чужой", 1);
+        Pokemon p2 = new Pokemon("Хищник", 1);
+        b.addAlly(p1);
+        b.addFoe(p2);
+        b.go();
     }
 }
--- Labs/Prog/Lab2/Lab2/src/moves/BrutalSwing.java
+++ Labs/Prog/Lab2/Lab2/src/moves/BrutalSwing.java
@@ -1,13 +1,25 @@
 package moves;
 
 import ru.ifmo.se.pokemon.PhysicalMove;
+import ru.ifmo.se.pokemon.Stat;
 import ru.ifmo.se.pokemon.Type;
+import ru.ifmo.se.pokemon.Pokemon;
 
 public class BrutalSwing extends PhysicalMove
 {
-    public BrutalSwing(Type type, double pow, double acc, int priority, int hits)
+    public BrutalSwing()
     {
-        super(Type.DARK, pow, acc, priority, hits);
+        super(Type.DARK, 60, 100);
+    }
+    @Override
+    protected void applySelfEffects(Pokemon p)
+    {
+        p.setMod(Stat.SPECIAL_ATTACK, 1);
+    }
 
+    @Override
+    protected String describe()
+    {
+    return "использует Brutal Swing";
     }
 }
