533966ed81c316904f56c877cda273edc972669d
ipka23 <ilyaipka@gmail.com>
2024-10-16 23:14:37 +0300

    dd

--- Labs/Inf/Lab3/regExp/task1.py
+++ Labs/Inf/Lab3/regExp/task1.py
@@ -1,12 +1,10 @@
 import re
+
+
 def count_regex(test):
     l = re.findall(r"X-\\", test)
-    return len(l)
+    return print(len(l))
 
-# test1 = "X-\\X-\\X-\\X-\\X-\\"
-# test2 = "abcX-\\defX-\\ghiX-\\jkl"
-# test3 = "X-\\\\X-\\X-\\\\X-\\"
-# test4 = "X-\\\\X_\\-\\X\\\\\\\\X-\\"
-# test5 = "12345!@#X-/xyz$%^&*()_+|X-/"
 
-print(count_regex(open('test1').readline()))
+for i in range(1,6):
+    count_regex(open(f'test{i}').readline())
