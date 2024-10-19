f16bd57217125d23571091ebefc818a2edfc0334
ipka23 <ilyaipka@gmail.com>
2024-10-19 23:43:03 +0300

    dd

--- Labs/Inf/Lab3/regExp/task3test.py
+++ Labs/Inf/Lab3/regExp/task3test.py
@@ -1,5 +1,11 @@
+month30 = ["apr", "jun", "sen", "nov", "4", "6", "9", "1"]
+after28 = ["29", "30", "31"]
+feb = ["feb", "2"]
 def cron(s):
-    if ((s.split()[3][:3] == "feb") + (s.split()[2][:2] == "29") + (s.split()[2][-2:] == "29")+ (s.split()[3][-3:] == "feb") + (s.split()[2][-2:] == "30") + (s.split()[2][:2] == "30")) >= 2:
-        return print(s.split()[3][:3])
-test = "* * 29 jan,feb *"
+    if ((s.split()[3][:3] in feb) + (s.split()[3][-3:] in feb) + (s.split()[3][0] in feb) + (s.split()[3][-1] in feb)) >= 1 \
+            and ((s.split()[2][:2] in after28) + (s.split()[2][-2:] in after28)) >= 1 \
+            or ((s.split()[3][:3] in month30) + (s.split()[3][-3:] in month30) + (s.split()[3][0] in month30) + (s.split()[3][-1] in month30) >= 1 \
+                and ((s.split()[2][:2] == "31") + (s.split()[2][-2:] == "31")) >= 1):
+        return print("There is no such date in this month!")
+test = "* * 31 1,apr *"
 cron(test)
