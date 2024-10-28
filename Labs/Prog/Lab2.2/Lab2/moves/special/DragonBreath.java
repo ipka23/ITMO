b4f3c93dee7f1c8be8f7411a34ae8a173dfb8298
ipka23 <ilyaipka@gmail.com>
2024-10-28 17:01:32 +0300

    dd

---
+++ Labs/Prog/Lab2.2/Lab2/moves/special/DragonBreath.java
@@ -0,0 +1,10 @@
+package moves.special;
+
+import java.lang.reflect.Type;
+
+public class DragonBreath {
+    private static final Type TYPE = Type.Dragon;
+    public DragonBreath(){
+        super();
+    }
+}
