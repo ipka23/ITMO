b465a1d0b68437528ad56d927fab893803353894
ipka23 <ilyaipka@gmail.com>
2024-11-02 17:07:29 +0300

    dd

--- Labs/Prog/Lab2.2/Lab2/moves/special/DragonBreath.java
+++ Labs/Prog/Lab2.2/Lab2/moves/special/DragonBreath.java
@@ -1,10 +1,23 @@
 package moves.special;
 
-import java.lang.reflect.Type;
 
-public class DragonBreath {
-    private static final Type TYPE = Type.Dragon;
+import ru.ifmo.se.pokemon.*;
+
+public class DragonBreath extends SpecialMove {
+    private static final Type TYPE = Type.DRAGON;
+    private static final double POWER = 60;
+    private static final double ACCURACY = 100;
     public DragonBreath(){
-        super();
+        super(TYPE, POWER, ACCURACY);
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon pokemon){
+        Effect paralyze = new Effect().chance(0.3).condition(Status.PARALYZE);
+        pokemon.addEffect(paralyze);
+    }
+    @Override
+    public String describe(){
+        return "использует Dragon Breath";
     }
 }
