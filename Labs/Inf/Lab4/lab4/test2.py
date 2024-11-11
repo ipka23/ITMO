e34c37677e0761e65e8665e71dd110552cbe2fb0
ipka23 <ilyaipka@gmail.com>
2024-11-12 00:00:49 +0300

    dd

--- Labs/Inf/Lab4/lab4/test2.py
+++ Labs/Inf/Lab4/lab4/test2.py
@@ -1,11 +1,17 @@
-path = "C:/Users/ilyai/OneDrive/Рабочий стол/ITMO/Labs/Inf/Lab4/lab4/schedule.yml"
+path = "C:/Users/ilyai/OneDrive/Рабочий стол/ITMO/Labs/Inf/Lab4/lab4/test2.yml"
 file = open(path, 'r', encoding='utf-8')
-yaml_lines = [line.rstrip() for line in file.readlines()]
-# for l in xml_lines:
-#     print(l)
-
-
-
+yaml_lines = [
+    "name: John Doe",
+    "age: 30",
+    "hobbies:",
+    "- reading",
+    "- traveling",
+    "education:",
+    "- degree: Bachelor",
+    "  major: Computer Science",
+    "- degree: Master",
+    "  major: Data Science"
+]
 def yaml_dict():
     d = {}
     for i in range(len(yaml_lines) - 2): # идём до конца файла(до последней строки) xml_index <= 81 number_of_xml_lines = 82
@@ -16,19 +22,4 @@ def yaml_dict():
             d[key] = value
         if yaml_lines[i+2].strip()[0] == "-" or i == len(yaml_lines) - 2:
             return d
-
-
-def xml_list():
-    global xml_index
-
-    res = []
-
-    while xml_index < len(yaml_lines):
-        xml_index += 1
-
-        if yaml_lines[xml_index] == '-':
-            res.append(xml_list())
-        elif yaml_lines[xml_index] == ']':
-            return res
-        else:
-            res.append(yaml_lines[xml_index])
+yaml_dict()
