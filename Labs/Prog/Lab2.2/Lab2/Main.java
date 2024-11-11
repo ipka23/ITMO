8e510aba07601ae43fa98354c45133a1435e5fcd
ipka23 <ilyaipka@gmail.com>
2024-11-11 10:09:52 +0300

    dd

--- Labs/Prog/Lab2.2/Lab2/Main.java
+++ Labs/Prog/Lab2.2/Lab2/Main.java
@@ -5,12 +5,12 @@ import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
-        Pokemon p1 = new Honedge("pokemon", 1);
-        Pokemon p2 = new Doublade("pokemon", 1);
-        Pokemon p3 = new AegislashBlade("pokemon", 1);
-        Pokemon p4 = new Swablu("pokemon", 1);
-        Pokemon p5 = new Altaria("pokemon", 1);
-        Pokemon p6 = new Miltank("pokemon", 1);
+        Pokemon p1 = new Honedge("Спичка", 1);
+        Pokemon p2 = new Doublade("Нож", 1);
+        Pokemon p3 = new AegislashBlade("Меч", 1);
+        Pokemon p4 = new Swablu("Говорун", 1);
+        Pokemon p5 = new Altaria("Летун", 1);
+        Pokemon p6 = new Miltank("Молоко", 1);
         Battle b = new Battle();
         b.addAlly(p1);
         b.addAlly(p2);
