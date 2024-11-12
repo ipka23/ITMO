ff52e14f1ccd35408ac8eb861cea32c92e05033d
ipka23 <ilyaipka@gmail.com>
2024-11-12 22:37:29 +0300

    dd

--- Labs/Inf/Lab4/lab4/test2.py
+++ Labs/Inf/Lab4/lab4/test2.py
@@ -1,59 +1,58 @@
 path = "test2.yml"
 file = open(path, 'r', encoding='utf-8')
-yaml_lines = [line.strip("-") for line in file.readlines()]
-
+yaml_lines = [line.strip("- ").replace('"', "").strip() for line in file.readlines()]
+i = 0
+# for l in yaml_lines:
+#     print(l)
 # def yaml_dict():
 #     d = {}
-#     for i in range(len(yaml_lines) - 1):
-#         if len(yaml_lines[i+1].split(': ')) == 1:  # проверяем является ли строка концом списка
+#     open_tags = []
+#     global i
+#     while i < len(yaml_lines) - 2:
+#         i += 1
+#         if len(yaml_lines[i+1].split(': ')) == 1 or i == len(yaml_lines):  # проверяем является ли строка концом списка
 #             return d
 #         key_and_value = yaml_lines[i].split(': ')
 #         key = key_and_value[0]
 #         value = key_and_value[1] if len(key_and_value) > 1 else ""
-#         if len(yaml_lines[i].split(': ')) == 1:  # проверяем является ли строка началом списка
+#         if len(yaml_lines[i].split(': ')) == 1: # проверяем является ли строка началом списка
+#             open_tags.append(yaml_lines[i].split(': ')[0])
 #             d[key] = yaml_dict()
 #         else:
 #             d[key] = value
-#         return d
+        # print(f'<{key}>{value}</{key}>')
+#         print(d, open_tags)
 # yaml_dict()
-def yaml_list():
-    l = []
-    for i in range(len(yaml_lines) - 1):
-        if len(yaml_lines[i].split(': ')) == 1:
-            l.append(yaml_list())
-        elif len(yaml_lines[i+1].split(': ')) == 1 or i == len(yaml_lines) - 1:  # проверяем является ли строка концом списка
-            return l
-        else:
-            l.append(yaml_lines[i])
 
-yaml_list()
-#
-# path = "test2.yml"
-# with open(path, 'r', encoding='utf-8') as file:
-#     yaml_lines = [line.strip() for line in file.readlines()]  # Убираем пробелы и символы
-#
-#
 # def yaml_list():
 #     l = []
-#     i = 0  # Индекс для прохода по yaml_lines
-#     while i < len(yaml_lines):
-#         line = yaml_lines[i].strip()
-#
-#         if line.startswith("-"):  # Проверяем, является ли строка элементом списка
-#             item = line[1:].strip()  # Убираем символ '-'
-#             l.append(item)  # Добавляем элемент в список
-#         elif ':' in line:  # Если строка содержит ':', это ключ-значение
-#             key_value = line.split(': ', 1)  # Разделяем по первому ':'
-#             key = key_value[0]
-#             value = key_value[1].strip() if len(key_value) > 1 else ""
-#             l.append({key: value})  # Добавляем словарь с ключом и значением
-#         else:
-#             break  # Выходим из цикла, если строка не соответствует формату
-#
-#         i += 1  # Увеличиваем индекс
-#
-#     return l
+#     for i in range(len(yaml_lines) - 1):
+#         if len(yaml_lines[i].split(': ')) == 1:
+#             l.append(yaml_list())
+#         elif len(yaml_lines[i+1].split(': ')) == 1 or i == len(yaml_lines) - 1:  # проверяем является ли строка концом списка
+#             return l
+#         elif len(yaml_lines[i].split(': ')) > 1:
+#             l.append(yaml_lines[i])
 #
 #
-# result = yaml_list()
-# print(result)
+# yaml_list()
+
+
+def json_dict():
+    global i
+
+    res = {}
+
+    while i < len(yaml_lines) - 2:
+        i += 1
+        if len(yaml_lines[i+1].split(':', )) == 1 or i + 1 == len(yaml_lines):
+            return res
+        if ":" not in yaml_lines[i]:
+            yaml_lines[i] =   ,]
+        key, value = yaml_lines[i].split(':', 1)
+        if value == '':
+            res[key] = json_dict()
+        else:
+            res[key] = value
+        print(res)
+json_dict()
