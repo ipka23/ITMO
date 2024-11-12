cb933e64e361561c825073af5b28e17f30f74d34
ipka23 <ilyaipka@gmail.com>
2024-11-12 17:17:51 +0300

    dd

--- Labs/Inf/Lab4/lab4/required task.py
+++ Labs/Inf/Lab4/lab4/required task.py
@@ -1,34 +1,36 @@
-path = "C:/Users/ilyai/OneDrive/Рабочий стол/ITMO/Labs/Inf/Lab4/lab4/schedule.yml"
+path = "schedule.yml"
 file = open(path, 'r', encoding='utf-8')
-yaml_lines = [line.rstrip() for line in file.readlines()]
-# for l in xml_lines:
-#     print(l)
+yaml_lines = [line.rstrip().strip("-\"") for line in file.readlines()]
+for l in yaml_lines:
+    print(l)
+i = 0
+
+#
+# def yaml_dict():
+#     global i
+#     d = {}
+#     while i < len(yaml_lines) - 2:
+#         i += 1 # идём до конца файла(до последней строки) xml_index <= 81 number_of_xml_lines = 82
+#         if yaml_lines[i+2].strip()[0] == "-" or i == len(yaml_lines) - 2:
+#             return d
+#         key, value = yaml_lines[i].split(': ', maxsplit=1)
+#         if yaml_lines[i].strip()[0] == "-":  # проверяем является ли строка началом списка
+#             d[key] = yaml_dict()
+#         else:
+#             d[key] = value
+# yaml_dict()
 
 
-
-def yaml_dict():
-    d = {}
-    for i in range(len(yaml_lines) - 2): # идём до конца файла(до последней строки) xml_index <= 81 number_of_xml_lines = 82
-        key, value = yaml_lines[i].split(': ')
-        if yaml_lines[i].strip()[0] == "-":  # проверяем является ли строка началом списка
-            d[key] = yaml_dict()
-        else:
-            d[key] = value
-        if yaml_lines[i+2].strip()[0] == "-" or i == len(yaml_lines) - 2:
-            return d
-
-
-def xml_list():
-    global xml_index
-
-    res = []
-
-    while xml_index < len(yaml_lines):
-        xml_index += 1
-
-        if yaml_lines[xml_index] == '-':
-            res.append(xml_list())
-        elif yaml_lines[xml_index] == ']':
-            return res
-        else:
-            res.append(yaml_lines[xml_index])
+# def yaml_list():
+#     global i
+#     l = []
+#     while i < len(yaml_lines) - 2:
+#         i += 1
+#         if yaml_lines[i] == '-':
+#             l.append(yaml_list())
+#         elif yaml_lines[i+2].strip()[0] == "-" or i == len(yaml_lines) - 2:
+#             return l
+#         else:
+#             l.append(yaml_lines[i])
+#
+# yaml_list()
--- Labs/Inf/Lab4/lab4/test.py
+++ Labs/Inf/Lab4/lab4/test.py
@@ -1,7 +1,6 @@
-s = ""
-schedule_yaml = open('schedule.yml', mode="r", encoding='utf-8')
-schedule_xml = open('schedule.xml', mode='a', encoding='utf-8')
-lines = [line for line in schedule_yaml]
-for i in range(len(lines)):
-    line = lines[i]
-    print(line)
+s = 'lessons:'
+key_and_value = s.split(': ')
+key = key_and_value[0]
+value = key_and_value[1] if len(key_and_value) > 1 else ""  # Проверяем длину списка
+
+print(key, value, len(s.split(': ')))
