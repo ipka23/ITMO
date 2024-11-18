dae8187da3513c2db994d8857a8df9bef43992f3
ipka23 <ilyaipka@gmail.com>
2024-11-19 01:18:18 +0300

    dd

--- Labs/Inf/Lab4/lab4/parse_yaml_list_dict.py
+++ Labs/Inf/Lab4/lab4/parse_yaml_list_dict.py
@@ -1,3 +1,4 @@
+yml = '''
 days:
   - day: Понедельник
     disciplines:
@@ -20,3 +21,31 @@ days:
           - lesson: Практика
             time: "13:30-15:00"
   - day: Четверг
+'''
+
+def yaml(a: str) -> dict:
+    # Split the input, first by each line
+    lines = a.split("\n")
+    # Create an empty dict
+    d = {}
+    # Split the input, by colon
+    for line in lines:
+        if line != "": # Looking for extra \n's which result in empty lines
+            key = f"{line.split(':')[0]}"
+            val = f"{line.split(':')[1]}"
+        else:
+            continue
+        # Clean up whitespace around the value
+        val = val.lstrip()
+        val = val.rstrip()
+
+        # If value is detected as integer, typecast it from string to int
+        if val.isnumeric(): val = int(val)
+
+        # Produce a dictionary
+        d[key] = val
+
+    return d
+
+res = yaml("name: John\nage: 30\nheight: 180")
+print(res)
