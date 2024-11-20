6de0cc8041bf110c7a117322dca5d732329145cb
ipka23 <ilyaipka@gmail.com>
2024-11-20 23:43:16 +0300

    dd

--- Labs/OPD/Lab1/1-5.sh
+++ Labs/OPD/Lab1/1-5.sh
@@ -98,7 +98,7 @@ cd lab0
 wc -m */*e 2> /dev/null | > /tmp/lab0file
 ls graveler2 2>&1 | sort
 cat -n trubbish4 2> /dev/null | grep -vi "o$"
-cat -n duosion6 2> /dev/null | sort -r
+cat -n duosion6 2> /dev/null | sort -ir
 ls -Rl 2> /tmp/errors | grep -v ":$"| grep -v "^\s*$" | grep "e$" | sort -k2 | head -n 3 
 ls -l taillow0 2> /dev/null | sort
 cd ..
