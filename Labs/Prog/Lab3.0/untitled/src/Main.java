1060838524be5607ea083b77f16e224ef79702c7
ipka23 <ilyaipka@gmail.com>
2024-11-22 00:19:06 +0300

    dd

--- Labs/Prog/Lab3.0/untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        Tygra t = new Tygra();
-        t.standUp();
-    }
-}
--- Labs/Prog/Lab3.0/untitled/src/Pux.java
+++
@@ -1,5 +0,0 @@
-public class Pux extends Character {
-    public Pux(){
-
-    }
-}
