42b64dd7ac143ece5ebfa84f6f1e855d1784afac
ipka23 <ilyaipka@gmail.com>
2024-11-25 17:42:03 +0300

    dd

---
+++ Labs/Prog/Lab3/lab3/src/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String title;
+    private int pages;
+    public Book(String name) {
+        this.title = title;
+    }
+    public Book(int pages) {
+        this.pages = pages;
+    }
+    public Book(String name, int pages) {
+        this.title = title;
+        this.pages = pages;
+    }
+    public String getTitle(){
+        return title;
+    }
+}
--- Labs/Prog/Lab3/lab3/src/Playable.java
+++ Labs/Prog/Lab3/lab3/src/Playable.java
@@ -1,3 +1,3 @@
 public interface Playable {
-    void play();
+    public void play();
 }
