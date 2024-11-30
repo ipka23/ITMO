1b9780d21e80151b5073145c036bebd172d4022c
ipka23 <ilyaipka@gmail.com>
2024-11-30 20:34:05 +0300

    dd

--- Labs/Inf/Lab4/lab4/additional_task_1.py
+++ Labs/Inf/Lab4/lab4/additional_task_1.py
@@ -1,8 +1,9 @@
 import yaml
 import dict2xml
-
-yaml_file = open('schedule.yml', 'r', encoding="utf-8")
-yaml_dict = yaml.safe_load(yaml_file)
-with open('schedule.xml', 'w', encoding="utf-8") as xml_file:
+def add_1():
+    yaml_file = input()
+    yaml_dict = yaml.safe_load(yaml_file)
     result = dict2xml.dict2xml(yaml_dict)
-    xml_file.write(result)
+    print(result)
+
+add_1()
--- Labs/Inf/Lab4/lab4/additional_task_2.py
+++ Labs/Inf/Lab4/lab4/additional_task_2.py
@@ -1,34 +1,37 @@
 import re
-path = "schedule.yml"
-with open(path, "r", encoding="utf-8") as yaml_file:
-    yaml_lines = [line for line in yaml_file]
 i = 0
 s = ""
+def add_2():
+    with open("schedule.yml", "r", encoding="utf-8") as yaml_file:
+        yaml_lines = [line for line in yaml_file]
+    def strip(string):
+        return string.strip()
+    def to_xml():
+        global i
+        global s
+        while i < len(yaml_lines):
+            line = re.sub('\"', '', yaml_lines[i])
+            space = len(line) - len(strip(line))
 
+            match_list = re.fullmatch(r'(\w+):', strip(line))  # начало списка
+            match_element = re.fullmatch(r'(\w+):( \w+.+)', strip(line)) # элемент списка
 
-def to_xml():
-    global i
-    global s
-    while i < len(yaml_lines):
-        line = re.sub('\"', '', yaml_lines[i])
-        space = len(line) - len(line.strip())
-        match_list = re.fullmatch(r'(\w+):', line.strip())  # начало списка
-        match_element = re.fullmatch(r'(\w+):( \w+.+)', line.strip())
+            if match_list:
+                list_name = match_list.group(1)
+                s += space * ' ' + f'<{list_name}>\n'
+                i += 1
+                to_xml()
+                s += space * ' ' + f'</{list_name}>\n'
 
-        if match_list:
-            list_name = match_list.group(1)
-            s += space * ' ' + f'<{list_name}>\n'
+            elif match_element:  # элемент списка
+                element_name = match_element.group(1)
+                element_value = match_element.group(2).strip()
+                s += space * ' ' + f'<{element_name}>{element_value}</{element_name}>\n'
             i += 1
-            to_xml()
-            s += space * ' ' + f'</{list_name}>\n'
+        return strip(s)
 
-        elif match_element:  # элемент списка
-            element_name = match_element.group(1)
-            element_value = match_element.group(2).strip()
-            s += space * ' ' + f'<{element_name}>{element_value}</{element_name}>\n'
-        i += 1
-    return s.strip()
+    with open('schedule.xml', 'w', encoding='utf-8') as xml_file:
+        result = to_xml()
+        xml_file.write(result)
 
-with open('schedule.xml', 'w', encoding='utf-8') as xml_file:
-    result = to_xml()
-    xml_file.write(result)
+add_2()
