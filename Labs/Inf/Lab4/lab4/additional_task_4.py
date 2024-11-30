2b7cef121c853ef47ce090b95c938d3bf070c5af
ipka23 <ilyaipka@gmail.com>
2024-11-30 20:34:05 +0300

    dd

--- Labs/Inf/Lab4/lab4/additional_task_4.py
+++ Labs/Inf/Lab4/lab4/additional_task_4.py
@@ -1,34 +1,37 @@
 import time
-import subprocess
+import required_task
+import additional_task_1
+import additional_task_2
+import additional_task_3
 
 start_time = time.perf_counter()
-for i in range(100):
-    subprocess.run(["/usr/local/bin/python3.12", "required_task.py"])
+for i in range(1000):
+    required_task.req_task()
 end_time = time.perf_counter()
 execution_time = end_time - start_time
-print(f"Время выполнения required_task: {round(execution_time, 3)} секунд")
+print(f"Время выполнения required_task: {round(execution_time, 10)} секунд")
 
-
-
-start_time = time.perf_counter()
-for i in range(100):
-    subprocess.run(["/usr/local/bin/python3.12", "additional_task_1.py"])
-end_time = time.perf_counter()
-execution_time = end_time - start_time
-print(f"Время выполнения additional_task_1: {round(execution_time, 3)} секунд")
 
 
 start_time = time.perf_counter()
-for i in range(100):
-    subprocess.run(["/usr/local/bin/python3.12", "additional_task_2.py"])
+for i in range(1000):
+    additional_task_1.add_1()
 end_time = time.perf_counter()
 execution_time = end_time - start_time
-print(f"Время выполнения additional_task_2: {round(execution_time, 3)} секунд")
+print(f"Время выполнения additional_task_1: {round(execution_time, 10)} секунд")
 
 
 start_time = time.perf_counter()
-for i in range(100):
-    subprocess.run(["/usr/local/bin/python3.12", "additional_task_3.py"])
+for i in range(1000):
+    additional_task_2.add_2()
 end_time = time.perf_counter()
 execution_time = end_time - start_time
-print(f"Время выполнения additional_task_3: {round(execution_time, 3)} секунд")
+print(f"Время выполнения additional_task_2: {round(execution_time, 10)} секунд")
+
+
+start_time = time.perf_counter()
+for i in range(1000):
+    additional_task_3.add_3()
+end_time = time.perf_counter()
+execution_time = end_time - start_time
+print(f"Время выполнения additional_task_3: {round(execution_time, 10)} секунд")
