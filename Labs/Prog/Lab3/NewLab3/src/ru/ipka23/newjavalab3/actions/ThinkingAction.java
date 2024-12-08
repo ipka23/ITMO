a0cfb26ae81baba769a5744c9bc8809e518f99b4
ipka23 <ilyaipka@gmail.com>
2024-12-08 23:59:11 +0300

    dd

--- Labs/Prog/Lab3/NewLab3/src/ru/ipka23/newjavalab3/actions/ThinkingAction.java
+++ Labs/Prog/Lab3/NewLab3/src/ru/ipka23/newjavalab3/actions/ThinkingAction.java
@@ -2,7 +2,7 @@ package ru.ipka23.newjavalab3.actions;
 
 import ru.ipka23.newjavalab3.characters.Neznayka;
 import ru.ipka23.newjavalab3.classes.Effort;
-import ru.ipka23.newjavalab3.classes.Letter;
+
 import ru.ipka23.newjavalab3.page.NotebookPage;
 
 import java.util.List;
