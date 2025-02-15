478368b0fce6307602b2d5b35276f4b52a055b1d
ipka23 <ilyaipka@gmail.com>
2025-02-15 20:15:24 +0300

    dd

---
+++ Subj/PROG/Labs/Lab5.0/Lab__5/lab5/src/main/java/Commands/Add.java
@@ -0,0 +1,36 @@
+package Commands;
+
+import managers.CollectionManager;
+import models.Ask;
+import models.MusicBand;
+import utility.Console;
+import utility.ExecutionResponse;
+
+public class Add extends Command {
+    Console console;
+    CollectionManager collectionManager;
+
+    public Add(Console console, CollectionManager collectionManager) {
+        super("add {element}", "добавить новый элемент в коллекцию");
+        this.console = console;
+        this.collectionManager = collectionManager;
+    }
+
+    @Override
+    public ExecutionResponse execute(String[] args) {
+        try {
+            if (args.length > 1) return new ExecutionResponse(false, "Неправильное количество аргументов!\nИспользование: '" + getName() + "'");
+            console.println("Cоздание новой MusicBand");
+            MusicBand band = Ask.askMusicBand(console, collectionManager.getFreeId());
+
+            if (band != null && band.isValid()) {
+                collectionManager.add(band);
+                return new ExecutionResponse(true, "MusicBand была успешно добалена");
+            } else {
+                return new ExecutionResponse(false,"Поля MusicBand не валидны, MusicBand не создана!");
+            }
+        } catch (Ask.AskBreak e) {
+            return new ExecutionResponse(false, "Отмена ввода...");
+        }
+    }
+}
