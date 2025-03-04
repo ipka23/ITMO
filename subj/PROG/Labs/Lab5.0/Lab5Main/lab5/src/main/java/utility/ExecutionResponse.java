e08a7ddb9a0103aee336fbec6e07fa85d8ffa4aa
ipka23 <ilyaipka@gmail.com>
2025-03-04 03:00:08 +0300

    dd

--- subj/PROG/Labs/Lab5.0/Lab5Main/lab5/src/main/java/utility/ExecutionResponse.java
+++ subj/PROG/Labs/Lab5.0/Lab5Main/lab5/src/main/java/utility/ExecutionResponse.java
@@ -1,21 +1,49 @@
 package utility;
 
+/**
+ * Данный класс отвечает за вывод состояния выполнения команды.
+ * Он содержит статус завершения и сообщение, описывающее результат выполнения команды.
+ *
+ * @author ipka23
+ */
 public class ExecutionResponse {
     boolean exitStatus;
     String message;
 
+    /**
+     * Конструктор класса ExecutionResponse.
+     *
+     * @param exitStatus статус завершения команды (true, если команда выполнена успешно, false в противном случае)
+     * @param message    сообщение, описывающее результат выполнения команды
+     */
     public ExecutionResponse(boolean exitStatus, String message) {
         this.exitStatus = exitStatus;
         this.message = message;
     }
 
+    /**
+     * Конструктор класса ExecutionResponse.
+     *
+     * @param exitStatus статус завершения команды (true, если команда выполнена успешно, false в противном случае)
+     */
     public ExecutionResponse(boolean exitStatus) {
         this.exitStatus = exitStatus;
     }
 
+    /**
+     * Метод для возвращения статуса завершения команды
+     *
+     * @return true, если команда выполнена успешно, false в противном случае
+     */
     public boolean getExitStatus() {
         return exitStatus;
     }
+
+    /**
+     * Метод для возвращения сообщения, описывающего результат выполнения команды
+     *
+     * @return сообщение о результате выполнения команды
+     */
     public String getMessage() {
         return message;
     }
--- subj/PROG/Labs/Lab5.0/Lab5Main/lab5/src/main/java/utility/StandartConsole.java
+++ subj/PROG/Labs/Lab5.0/Lab5Main/lab5/src/main/java/utility/StandartConsole.java
@@ -3,63 +3,101 @@ package utility;
 
 import java.util.Scanner;
 
-public class StandartConsole implements Console{
+/**
+ * Данный класс отвечает за работу стандартной консоли (ввод команд и вывод результата)
+ *
+ * @author ipka23
+ */
+public class StandartConsole implements Console {
     private static final String PROMPT = "$ ";
     private static final String SCRIPT_PROMPT = "> ";
     private static Scanner fileScanner = null;
     private static Scanner consoleScanner = new Scanner(System.in);
 
 
-
+    /**
+     * Метод для вывода obj.toString() в консоль
+     *
+     * @param obj Объект для вывода в консоль
+     */
     @Override
     public void print(Object obj) {
         System.out.print(obj);
     }
 
+    /**
+     * Метод для вывода obj.toString() + \n в консоль
+     *
+     * @param obj Объект для вывода в консоль
+     */
     @Override
     public void println(Object obj) {
         System.out.println(obj);
     }
 
+    /**
+     * Метод для вывода Error: obj.toString() + \n в консоль
+     *
+     * @param obj Ошибка для вывода в консоль
+     */
+    @Override
+    public void printError(Object obj) {
+        System.err.println("Error: " + obj);
+    }
 
+    /**
+     * Метод вывода приглашения для ввода текущей консоли: "$ "
+     */
+    @Override
+    public void printPrompt() {
+        System.out.print(PROMPT);
+    }
+
+    /**
+     * Метод для получения промпта для исполняемого скрипта
+     *
+     * @return промпт для исполняемого скрипта
+     */
+    @Override
+    public String getScriptPrompt() {
+        return SCRIPT_PROMPT;
+    }
+
+    /**
+     * Метод для чтения следующей строки ввода
+     * Если выбран сканер файла, проверка производится в файле, иначе в консоли.
+     */
     @Override
     public String readln() {
         if (fileScanner == null) return consoleScanner.nextLine();
         return fileScanner.nextLine();
     }
 
+    /**
+     * Метод для проверки наличия следующей строки ввода
+     * Если выбран сканер файла, проверка производится в файле, иначе в консоли.
+     */
     @Override
     public boolean hasNextLine() throws IllegalStateException {
         if (fileScanner == null) return consoleScanner.hasNextLine();
         return fileScanner.hasNextLine();
     }
 
-
-    @Override
-    public String getScriptPrompt() {
-        return SCRIPT_PROMPT;
-    }
-
+    /**
+     * Метод для выбора сканера консоли вместо сканера файла
+     */
     @Override
     public void selectConsoleScanner() {
         fileScanner = null;
     }
+
+    /**
+     * Метод для выбора сканера файла вместо сканера консоли
+     */
     @Override
-    public void selectFileScanner(Scanner scanner){
+    public void selectFileScanner(Scanner scanner) {
         fileScanner = scanner;
     }
 
 
-
-
-    @Override
-    public void printError(Object obj) {
-        System.err.println("Error: " + obj);
-    }
-
-
-    @Override
-    public void printPrompt() {
-        System.out.print(PROMPT);
-    }
 }
