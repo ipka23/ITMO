7159035b38aa3b8c3bb19c431c083bf30fc7342c
ipka23 <ilyaipka@gmail.com>
2025-03-10 13:54:20 +0300

    dd

--- subj/PROG/Labs/Lab5.0/newLab5/src/test/java/TestMain2.java
+++ subj/PROG/Labs/Lab5.0/newLab5/src/test/java/TestMain2.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TestMain2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for (;;){
-            String a = sc.nextLine();
-            System.out.println("Вы ввели: "+ a);
-        }
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("a");
+        list.add("b");
+        System.out.println(list.get(0) + " " + list.get(1));
     }
 }
 
