a4167f3cae8b368d8db6a2659488c8a01ddcfba1
ipka23 <ilyaipka@gmail.com>
2025-03-18 21:27:55 +0300

    dd

--- subj/PROG/Labs/Lab5.0/newLab5/src/main/java/utility/interfaces/Executable.java
+++
@@ -1,7 +0,0 @@
-package utility.interfaces;
-
-import utility.ExecutionResponse;
-
-public interface Executable {
-    ExecutionResponse execute(String[] userCommand);
-}
--- subj/PROG/Labs/Lab5.0/newLab5/src/main/java/utility/interfaces/Validatable.java
+++
@@ -1,7 +0,0 @@
-package utility.interfaces;
-
-import utility.exceptions.ValidateException;
-
-public interface Validatable {
-    boolean isValid() throws ValidateException;
-}
