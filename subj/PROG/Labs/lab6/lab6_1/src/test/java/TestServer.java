fb3734af84b92cc3d07830d39ceecd60329f2a14
ipka23 <ilyaipka@gmail.com>
2025-03-23 23:01:26 +0300

    dd

--- subj/PROG/Labs/lab6/lab6_1/src/test/java/TestServer.java
+++ subj/PROG/Labs/lab6/lab6_1/src/test/java/TestServer.java
@@ -1,26 +1,48 @@
+import utility.exceptions.ExitException;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TestServer {
     private static int PORT = 3232;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+    private static ServerSocket serverSocket;
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(PORT);
         System.out.println("Server started!");
-        while (!serverSocket.isClosed()) {
+        while (true) {
+            serverSocket = new ServerSocket(PORT);
             handleRequest(serverSocket);
         }
     }
     private static void handleRequest(ServerSocket serverSocket) throws IOException {
-        while (!serverSocket.isClosed()) {
-            Socket socket = serverSocket.accept();
+        Socket socket = serverSocket.accept();
+        System.out.println("Client connected!\nPort: " + PORT);
 
-            System.out.println("Client connected!\nPort: " + PORT);
+        try {
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            System.out.print("$ ");
+            while (true) {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                if (in.ready()) {
+                System.out.print("$ ");
+                String line = in.readLine();
+                if (line.equals("exit")) throw new ExitException();
+                out.write("Message from server: "  + line);
+                out.newLine();
+                out.flush();
+                }
+            }
+//            String messageFromClient = in.readLine();
+//            System.out.println(messageFromClient);
+//            out.write("Your message was sent successfully!");
+//            out.flush();
+        } catch (ExitException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            socket.close();
         }
     }
 }
