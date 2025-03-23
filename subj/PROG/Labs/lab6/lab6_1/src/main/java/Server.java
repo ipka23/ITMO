33b09d90a6e196708ee8a27c7f01c34e574bc5c4
ipka23 <ilyaipka@gmail.com>
2025-03-24 01:40:59 +0300

    dd

--- subj/PROG/Labs/lab6/lab6_1/src/main/java/Server.java
+++ subj/PROG/Labs/lab6/lab6_1/src/main/java/Server.java
@@ -1,9 +1,5 @@
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
     }
 }
