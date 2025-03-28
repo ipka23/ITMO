7b8e3bd3b7f3db112c5b0f66230352cc42eaf899
ipka23 <ilyaipka@gmail.com>
2025-03-29 00:19:48 +0300

    dd

--- subj/PROG/Labs/lab6/lab6_2/src/main/java/Main.java
+++ subj/PROG/Labs/lab6/lab6_2/src/main/java/Main.java
@@ -4,12 +4,14 @@ import managers.CommandManager;
 import managers.FileManager;
 import utility.Invoker;
 import utility.consoles.AdvancedConsole;
+import utility.consoles.StandartConsole;
 import utility.interfaces.Console;
 
 import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Введите имя файла как аргумент командной строки!");
@@ -22,33 +24,20 @@ public class Main {
             System.exit(2);
         }
 
-        Console console = new AdvancedConsole();
 
-        FileManager fileManager = new FileManager();
-        fileManager.setFile(args[0]);
-
-        CollectionManager collectionManager = new CollectionManager(fileManager);
+        Console console = new StandartConsole();
+        FileManager fileManager = new FileManager(null, console, args[0]);
+        CollectionManager collectionManager = new CollectionManager(fileManager, console);
+        CommandManager commandManager = new CommandManager(console, collectionManager);
+        Invoker invoker = new Invoker(commandManager, console);
 
         fileManager.setCollectionManager(collectionManager);
-
-        Invoker invoker = new Invoker(null, console);
-
-        CommandManager commandManager = new CommandManager(console, collectionManager);
+        console.setCollectionManager(collectionManager);
+        console.setInvoker(invoker);
 
         commandManager.addCommand("execute_script", new ExecuteScript(console, invoker));
 
-        invoker.setCommandManager(commandManager);
-        console.setCommandManager(commandManager);
-        console.setInvoker(invoker);
-
-        console.setCollectionManager(collectionManager);
-        console.setScanner(new Scanner(System.in));
-
-        collectionManager.setConsole(console);
         console.launch();
 
-//        Client.main(new String[]{"MusicBands.json"});
-
-//        Server server = new Server()
     }
 }
--- subj/PROG/Labs/lab6/lab6_2/src/main/java/managers/CommandManager.java
+++ subj/PROG/Labs/lab6/lab6_2/src/main/java/managers/CommandManager.java
@@ -34,6 +34,23 @@ public class CommandManager {
         commandsMap.put("update", new Update(console, collectionManager));
     }
 
+    public CommandManager() {
+        commandsMap.put("add", new Add(console, collectionManager));
+        commandsMap.put("add_if_max", new AddIfMax(console, collectionManager));
+        commandsMap.put("add_if_min", new AddIfMin(console, collectionManager));
+        commandsMap.put("clear", new Clear(console, collectionManager));
+        commandsMap.put("exit", new Exit());
+        commandsMap.put("filter_starts_with_name", new FilterStartsWithName(console, collectionManager));
+        commandsMap.put("help", new Help(console, this));
+        commandsMap.put("info", new Info(console, collectionManager));
+        commandsMap.put("max_by_best_album", new MaxByBestBestAlbum(console, collectionManager));
+        commandsMap.put("print_field_ascending_establishment_date", new PrintFieldAscendingEstablishmentDate(console, collectionManager));
+        commandsMap.put("remove_by_id", new RemoveByID(console, collectionManager));
+        commandsMap.put("remove_greater", new RemoveGreater(console, collectionManager));
+        commandsMap.put("show", new Show(console, collectionManager));
+        commandsMap.put("update", new Update(console, collectionManager));
+    }
+
 
     public void setCommandsMap(Map<String, Command> commandsMap) {
         this.commandsMap = commandsMap;
