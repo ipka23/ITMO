d14ecf8096dc543b253b1fe685b2debd4beb6659
ipka23 <ilyaipka@gmail.com>
2025-03-30 23:59:08 +0300

    dd

---
+++ subj/PROG/Labs/lab6/client/src/main/java/Command.java
@@ -0,0 +1,20 @@
+public abstract class Command implements Executable {
+    private String name;
+    private String description;
+//    private boolean hasArguments;  boolean hasArguments
+
+    public Command(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract ExecutionResponse execute(String[] command);
+}
