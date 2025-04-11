d42af49f51d5c8bad6ad562d8527de97ee2ce079
ipka23 <ilyaipka@gmail.com>
2025-04-12 01:21:12 +0300

    multiply x2 almost done

--- subj/OPD/Labs/lab5/dop(calculator).asm
+++ subj/OPD/Labs/lab5/dop(calculator).asm
@@ -15,103 +15,82 @@
 ;sign:                    word ?
 
 ; TODO реализовать умножение на 0 и добавить проверку на 0 в каждом умножении
-minus: word 0xA ; "-"
-a:  word ?
-sign: word 0x0
+sign:   word 0x0     ; 0 = положительное, 1 = отрицательное
+minus:  word 0xA     ; код символа "-"
+a:      word 0x0
 START:
-; сброс разрядов индикатора
+    cla
+    ; сброс разрядов индикатора
     ld #0x002B
     out 0x14
     ld #0x001B
     out 0x14
     ld #0x000B
     out 0x14
-    ;clc
-sign_input_1:
-    in 0x1C           ; ввод с цифровой клавиатуры
-    cmp minus         ; символ == "-"?
-    ;clc                ; т.к. при cmp если символ не равен "-" то при AC - m => С=1
-    bne skip_in_a       ; если не "-", то знак не меняем
-    ;cmc               ; меняем флаг C который указвает на знак цифры
-    ld sign
-    not
-    st sign
-
-
-a_input:
-    in 0x1C           ; ввод с цифровой клавиатуры
-skip_in_a:
-    cmp #0x0          ; начало проверки на a_input в {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-    beq break_a_input
+; цикл ввода a
+input_a:
+    in 0x1C
+    in 0x1C
+    cmp minus
+    beq handle_minus_a ;
     cmp #0x1
-    beq break_a_input
-    cmp #0x2
-    beq break_a_input
-    cmp #0x3
-    beq break_a_input
-    cmp #0x4
-    beq break_a_input
-    cmp #0x5
-    beq break_a_input
-    cmp #0x6
-    beq break_a_input
-    cmp #0x7
-    beq break_a_input
-    cmp #0x8
-    beq break_a_input
-    cmp #0x9
-    beq break_a_input ; конец проверки на a_input в {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-    jump a_input
-break_a_input:
+    blt input_a   ; если in < 0 то не цифра
+    cmp #0xB
+    bge input_a   ; если in >= B то не цифра и не минус
+
+
     st a ; сохраняем первый символ - a
+    jump multiply_input
+
+handle_minus_a:
+    ld sign
+    cmp #0x1
+    beq set_zero_a
+    ld #0x1
+    jump save_sign_a
+set_zero_a:
+    ld #0x0
+save_sign_a:
+    st sign
+    jump input_a
 
 
-jump multiply_input
 multiplication:   word 0xD ; "*"
 multiply_input:
     in 0x1C    ; ввод с клавиатуры
     cmp multiplication
-    beq sign_input_2
+    beq input_b
     jump multiply_input
 
 
 
-b: word ?
-sign_input_2:
-    in 0x1C           ; ввод с цифровой клавиатуры
-    cmp minus         ; символ == "-"?
-    bne skip_in_b     ; если не "-", то проверяем цифра ли это
-    ;cmc               ; меняем флаг C который указвает на знак цифры
-    ld sign
-    not
-    st sign
-
-b_input:
-    in 0x1C           ; ввод с цифровой клавиатуры
-skip_in_b:
-    cmp #0x0          ; начало проверки на b_input в {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-    beq break_b_input
+b:      word 0x0
+; цикл ввода b
+input_b:
+    in 0x1C
+    in 0x1C
+    cmp minus
+    beq handle_minus_b ;
     cmp #0x1
-    beq break_b_input
-    cmp #0x2
-    beq break_b_input
-    cmp #0x3
-    beq break_b_input
-    cmp #0x4
-    beq break_b_input
-    cmp #0x5
-    beq break_b_input
-    cmp #0x6
-    beq break_b_input
-    cmp #0x7
-    beq break_b_input
-    cmp #0x8
-    beq break_b_input
-    cmp #0x9
-    beq break_b_input ; конец проверки на b_input в {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-    jump b_input
-break_b_input:
-    st b ; сохраняем второй символ - b
+    blt input_b   ; если in < 0 то не цифра
+    cmp #0xB
+    bge input_b   ; если in >= B то не цифра и не минус
+
+
+    st b ; сохраняем первый символ - a
+    jump equate_input
+
+handle_minus_b:
+    ld sign
+    cmp #0x1
+    beq set_zero_b
+    ld #0x1
+    jump save_sign_b
+set_zero_b:
+    ld #0x0
+save_sign_b:
+    st sign
+    jump input_b
 
 
 
@@ -119,11 +98,12 @@ jump equate_input
 equate: word 0xF ; "="
 equate_input:
     in 0x1C
+    in 0x1C
     cmp equate
     beq multiply
     jump equate_input
 
-res: word ?
+res:    word 0x0
 multiply:
     ld a
     push
@@ -141,16 +121,21 @@ multiply:
 
 FINISH:
     ld sign
-    rol
+    cmp #0x1
+    beq negative_res
+
     ld res
-    bcs negative_num ; я не знаю почему bcc а не bcs но оно работает, а нет не работает
-    out 0x14
+    out 0x14         ; выводим цифру на позицию 0
     hlt
-negative_num:
-    out 0x14   ; вывод res на позицию №0
-    ld #0x001A ;
-    out 0x14   ; вывод "-" на позицию №1 на 7-сегментном индикаторе
+
+negative_res:
+    ld #0x001A
+    out 0x14         ; выводим "-" на позицию 1
+    ld res
+    out 0x14         ; выводим цифру на позицию 0
     hlt
+
+
 
 
 org 0x200
@@ -158,25 +143,52 @@ func:
     ld &2 ; a -> AC
     cmp #0x1
     beq multiply_X1
+    cmp #0x2
+    beq multiply_X2
     ld &1 ; b -> AC
     cmp #0x1
     beq multiply_X1
+    cmp #0x2
+    beq multiply_X2
 
 
 ; a = &2 - первый множитель, b = &1 - второй множитель
 multiply_X1:
     ld &1 ; в AC записываем b
     cmp #0x1
-    beq return_a
+    beq return_aX1
     st &2 ; если b != 1, то записываем его в &2
     ret   ; и делаем ret (return b)
-return_a:
+return_aX1:
     ret
 
 
 
 multiply_X2:
+    ld &1 ; в AC записываем b
+    cmp #0x2
+    beq return_aX2
+    asl   ;
+    st &2 ; если b != 2, то умножаем его на 2 и записываем в 7FF = &2
+    ret   ; и делаем ret (return b)
+return_aX2:
+    asl
+    ret
+
+
 multiply_X3:
+    ld &1 ; в AC записываем b
+    cmp #0x3
+    beq return_aX3
+    asl   ;
+    add &1
+    st &2 ; если b != 3, то умножаем его на 3 и записываем в 7FF = &2
+    ret   ; и делаем ret (return b)
+return_aX3:
+    asl
+    add &2
+    ret
+
 
 
 to_decimal:
