db4d0b99d1fdc65caef135e118cbe5c2ddc9ae2d
ipka23 <ilyaipka@gmail.com>
2025-04-12 15:44:56 +0300

    hex to bcd done!

--- subj/OPD/Labs/lab5/dop(calculator).asm
+++ subj/OPD/Labs/lab5/dop(calculator).asm
@@ -13,11 +13,17 @@
 ;unsigned: word 0x0000
 ;mask_for_sign_definiton: word 0x7fff
 ;sign:                    word ?
-
 ; TODO реализовать умножение на 0 и добавить проверку на 0 в каждом умножении
-sign:   word 0x0     ; 0 = положительное, 1 = отрицательное
-minus:  word 0xA     ; код символа "-"
-a:      word 0x0
+
+a:                word 0x0 ; первый множитель
+b:                word 0x0 ; второй множитель
+res:              word 0x0 ; резульат умножения
+
+sign:             word 0x0 ; знак результата
+minus:            word 0xA ; код символа "-"
+equate:           word 0xF ; код символа "="
+multiplication:   word 0xD ; код символа "*"
+
 START:
     cla
     ; сброс разрядов индикатора
@@ -27,44 +33,47 @@ START:
     out 0x14
     ld #0x000B
     out 0x14
+
+
+
+
 ; цикл ввода a
 input_a:
     in 0x1C
     in 0x1C
     cmp minus
-    beq handle_minus_a ;
+    beq handle_minus_a
     cmp #0x1
     blt input_a   ; если in < 0 то не цифра
     cmp #0xB
     bge input_a   ; если in >= B то не цифра и не минус
-
-
-    st a ; сохраняем первый символ - a
+    st a          ; сохраняем первый символ - a
     jump multiply_input
 
+; изменение знака
 handle_minus_a:
     ld sign
     cmp #0x1
     beq set_zero_a
-    ld #0x1
+    ld #0x1         ; установка "-"
     jump save_sign_a
-set_zero_a:
+set_zero_a:         ; установка "+"
     ld #0x0
-save_sign_a:
+save_sign_a:        ; сохранение знака переход к вводу a
     st sign
     jump input_a
 
 
-multiplication:   word 0xD ; "*"
+; цикл ввода "*"
 multiply_input:
     in 0x1C    ; ввод с клавиатуры
     cmp multiplication
+    bne multiply_input
     beq input_b
-    jump multiply_input
 
 
 
-b:      word 0x0
+
 ; цикл ввода b
 input_b:
     in 0x1C
@@ -75,35 +84,35 @@ input_b:
     blt input_b   ; если in < 0 то не цифра
     cmp #0xB
     bge input_b   ; если in >= B то не цифра и не минус
-
-
-    st b ; сохраняем первый символ - a
+    st b          ; сохраняем первый символ - a
     jump equate_input
 
+; изменение знака
 handle_minus_b:
     ld sign
     cmp #0x1
     beq set_zero_b
-    ld #0x1
+    ld #0x1           ; установка "-"
     jump save_sign_b
-set_zero_b:
+
+set_zero_b:           ; установка "+"
     ld #0x0
-save_sign_b:
+save_sign_b:          ; сохранение знака переход к вводу b
     st sign
     jump input_b
 
 
 
-jump equate_input
-equate: word 0xF ; "="
+; цикл ввода "="
 equate_input:
     in 0x1C
     in 0x1C
     cmp equate
     beq multiply
-    jump equate_input
+    bne equate_input
 
-res:    word 0x0
+
+; умножение
 multiply:
     ld a
     push
@@ -118,26 +127,54 @@ multiply:
 
 
 
-
+; вывод результата
 FINISH:
+    ld res
+    ;and #0x00f0               ; маска для сравнения 2 тетрады
+    cmp #0x0010
+    bge two_digits            ; если во второй тетраде число >= 1, значит двузначное число
+    blt one_digit             ; если во второй тетраде число < 1, значит цифра
+
+
+; вывод цифры
+one_digit:
     ld sign
     cmp #0x1
-    beq negative_res
-
+    beq negative_res_one_digit  ; если "-" то переходим к выводу цифры с минусом
     ld res
-    out 0x14         ; выводим цифру на позицию 0
+    out 0x14                    ; иначе выводим цифру на позицию 0 и завершаем программу //TODO можно в теории потом просто добавить jump START чтобы калькулятор работал без остановки, но тогда нужно будет еще обнулять знак
     hlt
 
-negative_res:
+; вывод двузначного числа
+two_digits:
+    ld sign
+    cmp #0x1
+    beq negative_res_two_digits  ; если "-" то переходим к выводу числа с минусом
+    ld res
+    and #0x00f0
+    cmp #0x0010
+    beq second_digit_out
+
+second_digit_out:
+
+
+; вывод цифры с минусом
+negative_res_one_digit:
     ld #0x001A
     out 0x14         ; выводим "-" на позицию 1
     ld res
     out 0x14         ; выводим цифру на позицию 0
     hlt
 
+; вывод двузначного числа с минусом
+negative_res_two_digits:
+    ld #0x002A
+    out 0x14         ; ; выводим "-" на позицию 2
 
 
 
+
+; функция умножения
 org 0x200
 func:
     ld &2 ; a -> AC
@@ -172,23 +209,7 @@ multiply_X2:
     st &2 ; если b != 2, то умножаем его на 2 и записываем в 7FF = &2
     ret   ; и делаем ret (return b)
 return_aX2:
+    ld &2
     asl
+    st &2
     ret
-
-
-multiply_X3:
-    ld &1 ; в AC записываем b
-    cmp #0x3
-    beq return_aX3
-    asl   ;
-    add &1
-    st &2 ; если b != 3, то умножаем его на 3 и записываем в 7FF = &2
-    ret   ; и делаем ret (return b)
-return_aX3:
-    asl
-    add &2
-    ret
-
-
-
-to_decimal:
