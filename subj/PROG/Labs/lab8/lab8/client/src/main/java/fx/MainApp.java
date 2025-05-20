2d3060ecc87e63fbd3e1053e9cd8a658512bcb10
ipka23 <ilyaipka@gmail.com>
2025-05-20 22:56:07 +0300

    login works

--- subj/PROG/Labs/lab8/lab8/client/src/main/java/fx/MainApp.java
+++ subj/PROG/Labs/lab8/lab8/client/src/main/java/fx/MainApp.java
@@ -2,19 +2,33 @@ package fx;
 
 import fx.controllers.SceneController;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import network.Client;
+import network.RequestSender;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainApp extends Application {
+
+    private static final ExecutorService executor = Executors.newFixedThreadPool(10);
+    private volatile SceneController sceneController;
+    private RequestSender requestSender;
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fx/auth.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fx/auth.fxml"));
+            Parent root = loader.load();
+            sceneController = loader.getController();
             stage.setResizable(false);
             Image icon = new Image("images/pokeball.png");
             stage.getIcons().add(icon);
@@ -25,7 +39,35 @@ public class MainApp extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        executor.submit(() -> {
+            try {
+                int port = 1232;
+                String hostName = "localhost";
+                Socket socket;
+                socket = new Socket(hostName, port);
+                Client client = new Client(port, hostName, socket);
+
+                ObjectOutputStream outToServer = new ObjectOutputStream(new BufferedOutputStream(client.getSocket().getOutputStream()));
+                outToServer.flush();
+
+                ObjectInputStream inFromServer = new ObjectInputStream(new BufferedInputStream(client.getSocket().getInputStream()));
+                requestSender = new RequestSender(outToServer, inFromServer);
+                Platform.runLater(() -> {
+                    sceneController.setRequestSender(requestSender);
+                });
+
+
+//                requestSender.authentication(outToServer, inFromServer);
+//                requestSender.sendMessage();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
     }
+
 
     public static void main(String[] args) {
         launch(args);
--- subj/PROG/Labs/lab8/lab8/client/src/main/java/fx/controllers/SceneController.java
+++ subj/PROG/Labs/lab8/lab8/client/src/main/java/fx/controllers/SceneController.java
@@ -2,38 +2,36 @@ package fx.controllers;
 
 import common_utility.database.User;
 import common_utility.network.Request;
+import common_utility.network.Response;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import network.RequestSender;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.security.Key;
 
-@NoArgsConstructor
 public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
-//    private RequestSender requestSender;
+    private RequestSender rs;
     private ObjectOutputStream outToServer;
     private ObjectInputStream inFromServer;
-    public SceneController(ObjectOutputStream outToServer, ObjectInputStream inFromServer) {
-        this.outToServer = outToServer;
-        this.inFromServer = inFromServer;
-    }
+
     @FXML
     private TextField usernameLogIn;
     @FXML
@@ -44,16 +42,28 @@ public class SceneController {
     private PasswordField registerPassword_1;
     @FXML
     private PasswordField registerPassword_2;
+    @FXML
+    private Label registerMessage;
+    @FXML
+    private Label logInMessage;
 
+
+    public SceneController() {
+
+    }
+
+    public void setRequestSender(RequestSender rs) {
+        this.rs = rs;
+        this.outToServer = rs.getOutToServer();
+        this.inFromServer = rs.getInFromServer();
+    }
     @FXML
     public void onLoginButtonClick(ActionEvent event) throws IOException {
-//        String username = usernameLogIn.getText();
-//        String password = logInPassword.getText();
-//        System.out.printf("%s %s", username, password);
-//        RequestSender.sendRequest(new Request("login", new User(username, password)), outToServer);
-        root = FXMLLoader.load(getClass().getResource("/fx/login.fxml"));
-        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fx/login.fxml"));
+        root = loader.load();
+        SceneController controller = loader.getController();
+        controller.setRequestSender(rs);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -61,15 +71,89 @@ public class SceneController {
 
     @FXML
     public void onRegisterButtonClick(ActionEvent event) throws IOException {
-//        String username = usernameRegister.getText();
-//        String password_1 = registerPassword_1.getText();
-//        String password_2 = registerPassword_2.getText();
-//        System.out.printf("%s %s %s", username, password_1, password_2);
-//        registerPassword_1.getOnKeyPressed().handle;
-        root = FXMLLoader.load(getClass().getResource("/fx/register.fxml"));
-        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fx/register.fxml"));
+        root = loader.load();
+        SceneController controller = loader.getController();
+        controller.setRequestSender(rs);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    public void backFromRegister(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fx/auth.fxml"));
+        root = loader.load();
+        SceneController controller = loader.getController();
+        controller.setRequestSender(rs);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void backFromLogin(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fx/auth.fxml"));
+        root = loader.load();
+        SceneController controller = loader.getController();
+        controller.setRequestSender(rs);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void completeLogin(ActionEvent event) throws IOException, ClassNotFoundException, InterruptedException {
+        String username = usernameLogIn.getText();
+        String password = logInPassword.getText();
+        System.out.printf("%s %s", username, password);
+        rs.sendRequest(new Request("login", new User(username, password)), outToServer);
+        Response response = rs.getResponse(inFromServer);
+        if (!response.getExitStatus()) {
+            logInMessage.setText(response.getMessage());
+            logInPassword.setText("");
+        } else {
+            logInMessage.setText(response.getMessage());
+            startMainApp(event);
+        }
+    }
+
+    private void startMainApp(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fx/main.fxml"));
+        root = loader.load();
+        SceneController controller = loader.getController();
+        controller.setRequestSender(rs);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void completeRegister(ActionEvent event) throws IOException, ClassNotFoundException {
+        String username = usernameRegister.getText();
+        String password_1 = registerPassword_1.getText();
+        String password_2 = registerPassword_2.getText();
+        if (!registerPassword_1.equals(registerPassword_2)) {
+            registerMessage.setText("Пароли не совпадают!"); //todo fix
+            registerPassword_1.setText("");
+            registerPassword_2.setText("");
+        } else {
+            rs.sendRequest(new Request("register", new User(username, password_1)), outToServer);
+            Response response = rs.getResponse(inFromServer);
+            if (!response.getExitStatus()) {
+                registerMessage.setText(response.getMessage());
+            } else {
+                registerMessage.setText(response.getMessage());
+            }
+            System.out.printf("%s %s %s", username, password_1, password_2);
+        }
+    }
+
+
 }
