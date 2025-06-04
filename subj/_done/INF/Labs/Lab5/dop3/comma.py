9699b550111c851632dbc6ad85a06c96e50931c4
ipka23 <ilyaipka@gmail.com>
2025-06-05 00:17:00 +0300

    dd

--- subj/_done/INF/Labs/Lab5/dop3/comma.py
+++
@@ -1,9 +0,0 @@
-file = open('data.csv')
-lines = [line.replace(';', ',') for line in file.readlines()]
-new_file = open('data2.csv', 'w')
-s = ""
-for i in range(len(lines)):
-    s += lines[i]
-new_file.write(s)
-file.close()
-new_file.close()
--- subj/_done/INF/Labs/Lab5/dop3/dop3.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-
-data_frame = pd.read_csv("data2.csv")
-figure, ax = plt.subplots(1, 4, figsize=(16, 8), sharey=True)
-g = data_frame.groupby(['<DATE>']).boxplot(column="<OPEN>,<HIGH>,<LOW>,<CLOSE>".split(","), ax=ax) # группировка данных в data_frame по уникальным значениям в столбце <DATE>
-# plt.show()
-figure.savefig('boxplot.png')
