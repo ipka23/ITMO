68dfdcc950ce7be054835345fcd0e526bcac1a74
ipka23 <ilyaipka@gmail.com>
2025-09-10 22:50:29 +0300

    added index.js

---
+++ 2 курс/Веб-программирование/lab1/index.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 8; i++) {
+    let x = document.getElementById(`x(${i})` );
+    if (x != null) break
+    console.log(x);
+}
+
+
+let y = document.getElementById("inputY");
+alert()
+console.log(y);
+
+let r = document.getElementById("inputR");
+console.log(r);
+
