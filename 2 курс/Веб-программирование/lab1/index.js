4a106b68406a211400863d4512b8c3670233b210
ipka23 <ilyaipka@gmail.com>
2025-09-11 22:34:57 +0300

    index.js done

--- 2 курс/Веб-программирование/lab1/index.js
+++ 2 курс/Веб-программирование/lab1/index.js
@@ -2,19 +2,19 @@ const submitButton = document.getElementById("submitButton")
 let x
 let y
 let r
-
 const form = document.getElementById("form")
 
 function radioClick(e) {
     // console.log(e.target.value)
-    x = e.target.value
+    x = +e.target.value
 }
 
 for (let i = 0; i < form.x.length; i++) {
     form.x[i].addEventListener("click", radioClick)
 }
 
-submitButton.onclick = function () {
+submitButton.onclick = function (e) {
+    e.preventDefault()
     if (x === undefined) {
         alert("Выберите X!")
     }
@@ -30,17 +30,40 @@ submitButton.onclick = function () {
         alert("Введите значение R в пределах (2;5)!")
     } else {
         console.log(`nums: ${x}, ${y}, ${r}\ntime:${currentTime}`)
-        alert("Топ")
+        hit(x, y, r)
+        updateTable(x, y, r, "попадание", "---")
     }
 
-    function hit(x, y) {
+    function hit(x, y, r) {
         const svg = document.getElementById("svg")
-        const centerX = 150
-        const centerY = 150
-        const dot = document.createElement("circle")
-        dot.x1 = centerX + x
-        
-        svg.appendChild()
+        const rPxSize = svg.clientWidth / 3
+        const svgCenterX = 150
+        const svgCenterY = 150
+        const scale = rPxSize / r
+        const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+        dot.setAttributeNS(null, "r", "1%")
+        dot.setAttributeNS(null, "cx", (svgCenterX + x * scale).toString())
+        dot.setAttributeNS(null, "cy", (svgCenterY - y * scale).toString())
+        dot.setAttributeNS(null, "fill", "red")
+        dot.setAttributeNS(null, "visibility", "visible")
+        svg.appendChild(dot)
+    }
+
+    function updateTable(x, y, r, status, executionTime) {
+        const table = document.getElementById("statsTable")
+        const row = table.insertRow()
+        const xCell = row.insertCell(0)
+        const yCell = row.insertCell(1)
+        const rCell = row.insertCell(2)
+        const statusCell = row.insertCell(3)
+        const dateCell = row.insertCell(4)
+        const executionTimeCell = row.insertCell(5)
+        xCell.innerHTML = x
+        yCell.innerHTML = y
+        rCell.innerHTML = r
+        statusCell.innerHTML = status
+        dateCell.innerHTML = new Date().toLocaleString()
+        executionTimeCell.innerHTML = executionTime
     }
 }
 
