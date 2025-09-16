9135ef3c3511754a30ffc2fbdb6505735862263f
ipka23 <ilyaipka@gmail.com>
2025-09-16 15:08:33 +0300

    dd

--- 2 курс/Веб-программирование/lab1/src/main/java/Server.java
+++ 2 курс/Веб-программирование/lab1/src/main/java/Server.java
@@ -3,6 +3,7 @@ import com.fastcgi.FCGIInterface;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -10,21 +11,39 @@ import java.util.Map;
 
 public class Server {
     public static void main(String[] args) {
+//
+        System.err.println("Java FCGI Server starting...");
+        System.err.println("Current dir: " + System.getProperty("user.dir"));
+//
         var fcgiInterface = new FCGIInterface();
+//
+        System.err.println("FCGI Interface created");
+//
         while (fcgiInterface.FCGIaccept() >= 0) {
+//
+            System.err.println("Request accepted");
+//
             var startTime = System.nanoTime();
             var method = FCGIInterface.request.params.getProperty("REQUEST_METHOD");
+//
+            System.err.println("method: " + method);
             if (method.equals("POST")) {
                 String requestBody;
                 try {
                     requestBody = getRequestBody();
+//
+                    System.err.println(requestBody);
+//
                 } catch (IOException e) {
-                    System.out.println(htmlErrorResponse("Ошибка ввода-вывода!"));
+                    System.out.println(jsonErrorResponse("Ошибка ввода-вывода!"));
                     continue;
                 }
-
+//
                 Map<String, String> coords = parseCoords(requestBody);
-
+                for (Map.Entry<String, String> entry : coords.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
+//
                 ValidateResponse validateCoords = CoordinatesValidator.validate(coords);
                 if (validateCoords.getStatus()) {
                     String hit;
@@ -34,21 +53,21 @@ public class Server {
 
                     var endTime = System.nanoTime();
 
-                    Date currentDate = Calendar.getInstance().getTime();
+                    Date currentTime = Calendar.getInstance().getTime();
                     String executionTime = (endTime - startTime) / 1_000_000 + "ms";
-                    String s = "X: " + coords.get("x") +
-                            "\nY: " + coords.get("y") +
-                            "\nR: " + coords.get("r") +
-                            "\nСтатус: " + hit +
-                            "\nТекущее время: " + currentDate +
-                            "\nВремя выполнения: " + executionTime;
+                    String s = "{\"x\":\"" + coords.get("x") +
+                            "\",\"y\":\"" + coords.get("y") +
+                             "\",\"r\":\"" + coords.get("r") +
+                            "\",\"status\":\"" + hit +
+                            "\",\"currentTime\":\"" + currentTime +
+                            "\",\"executionTime\":\"" + executionTime + "\"}";
 
-                    System.out.println(htmlSuccessResponse(s));
+                    System.out.println(jsonSuccessResponse(s));
                 } else {
-                    System.out.println(htmlErrorResponse(validateCoords.getMessage()));
+                    System.out.println(jsonErrorResponse(validateCoords.getMessage()));
                 }
             } else {
-                System.out.println(htmlErrorResponse("отправьте POST запрос вместо " + method + " запроса!"));
+                System.out.println(jsonErrorResponse("отправьте POST запрос вместо " + method + " запроса!"));
             }
 
         }
@@ -100,38 +119,24 @@ public class Server {
         return new String(buffer, 0, totalRead);
     }
 
-    private static String htmlSuccessResponse(String message) {
-        return """
-                Content-Type: text/html\r\n\r\n<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Веб лаба 1</title>
-                </head>
-                <body>
-                    <h1 style="color: green">Результат выполнения</h1>
-                    <p>""" + message + """
-                </p>
-                </body>
-                </html>
-                """;
+    private static String jsonSuccessResponse(String jsonStr) {
+        String s = "Content-Type: application/json\n" +
+                "Content-Length: " + jsonStr.getBytes(StandardCharsets.UTF_8).length +
+                "\r\n\r\n" + jsonStr;
+//
+        System.err.println(s);
+//
+        return s;
     }
 
-    private static String htmlErrorResponse(String message) {
-        return """
-                Content-Type: text/html\r\n\r\n<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Веб лаба 1</title>
-                </head>
-                <body>
-                    <h1 style="color: red">Ошибка выполнения</h1>
-                    <p>Описание:""" + message + """
-                </p>
-                </body>
-                </html>
-                """;
+    private static String jsonErrorResponse(String message) {
+        String s = "Content-Type: application/json\n" +
+                "Content-Length: " + message.getBytes(StandardCharsets.UTF_8).length +
+                "\r\n\r\n" + "{\"error\":\"" + message + "\"}";
+//
+        System.err.println(s);
+//
+        return s;
     }
 
     private static Map<String, String> parseCoords(String s) {
