6d065705915902eb8803e415b95beb33f6139dd6
ipka23 <ilyaipka@gmail.com>
2025-09-18 00:16:26 +0300

    dd

--- 2 курс/Веб-программирование/lab1/src/main/java/Server.java
+++ 2 курс/Веб-программирование/lab1/src/main/java/Server.java
@@ -1,52 +1,30 @@
 import com.fastcgi.FCGIInterface;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
     public static void main(String[] args) {
-//        System.err.println("Java FCGI Server starting...");
-//        System.err.println("Current dir: " + System.getProperty("user.dir"));
-//
         var fcgiInterface = new FCGIInterface();
-//
-//        System.err.println("FCGI Interface created");
-//
         while (fcgiInterface.FCGIaccept() >= 0) {
+            long startTime = System.nanoTime();
             boolean get = true;
-//
-//            System.err.println("Request accepted");
-//
-            var startTime = System.nanoTime();
             var method = FCGIInterface.request.params.getProperty("REQUEST_METHOD");
-//
-//            System.err.println("method: " + method);
-//
             Map<String, String> coords;
             if (method.equals("POST")) {
                 get = false;
                 String requestBody;
                 try {
                     requestBody = getRequestBody();
-//
-//                    System.err.println(requestBody);
-//
                 } catch (IOException e) {
                     System.out.println(jsonErrorResponse("Ошибка ввода-вывода!"));
                     continue;
                 }
                 coords = parseCoords(requestBody);
-//
-//                for (Map.Entry<String, String> entry : coords.entrySet()) {
-//                    System.out.println(entry.getKey() + ": " + entry.getValue());
-//                }
-//
             } else if (method.equals("GET")) {
                 var queryString = FCGIInterface.request.params.getProperty("QUERY_STRING");
                 coords = parseCoords(queryString);
@@ -61,10 +39,10 @@ public class Server {
                     hit = "Попадание";
                 } else hit = "Промах";
 
-                var endTime = System.nanoTime();
-
-                Date currentTime = Calendar.getInstance().getTime();
-                String executionTime = (endTime - startTime) / 1_000_000 + "ms";
+                long endTime = System.nanoTime();
+                String currentTime = new SimpleDateFormat("dd-MM-yyyy HH:mm").format(Calendar.getInstance().getTime());
+                double t = (endTime - startTime) / 1_000_000d;
+                String executionTime = String.format("%.2fms", t);
                 String s;
                 if (!get) {
                     s = "{\"x\":\"" + coords.get("x") +
@@ -74,8 +52,7 @@ public class Server {
                             "\",\"currentTime\":\"" + currentTime +
                             "\",\"executionTime\":\"" + executionTime + "\"}";
                     System.out.println(jsonSuccessResponse(s));
-                }
-                else {
+                } else {
                     s = "X: " + coords.get("x") +
                             "\nY: " + coords.get("y") +
                             "\nR: " + coords.get("r") +
@@ -87,46 +64,27 @@ public class Server {
             } else {
                 if (!get) {
                     System.out.println(jsonErrorResponse(validateCoords.getMessage()));
-                }
-                else {
+                } else {
                     System.out.println(htmlErrorResponse(validateCoords.getMessage()));
                 }
             }
         }
     }
 
-//    private static String getRequestBody() throws IOException {
-//        var contentLength = FCGIInterface.request.params.getProperty("CONTENT_LENGTH");
-//        try (InputStreamReader isr = new InputStreamReader(System.in, StandardCharsets.UTF_8);
-//             BufferedReader br = new BufferedReader(isr)) {
-//            StringBuilder buf = new StringBuilder(contentLength);
-//            int b;
-//            while ((b = br.read()) != -1) {
-//                buf.append((char) b);
-//            }
-//            return buf.toString();
-//        }
-//
-//    }
-
     private static String getRequestBody() throws IOException {
-        var contentLengthStr = FCGIInterface.request.params.getProperty("CONTENT_LENGTH");
+        String contentLengthStr = FCGIInterface.request.params.getProperty("CONTENT_LENGTH");
         if (contentLengthStr == null || contentLengthStr.isEmpty()) {
             return "";
         }
-
         int contentLength;
         try {
             contentLength = Integer.parseInt(contentLengthStr);
         } catch (NumberFormatException e) {
-            System.err.println("Invalid Content-Length: " + contentLengthStr);
             return "";
         }
-
         if (contentLength <= 0) {
             return "";
         }
-
         char[] buffer = new char[contentLength];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int totalRead = 0;
@@ -137,25 +95,18 @@ public class Server {
             }
             totalRead += read;
         }
-
         return new String(buffer, 0, totalRead);
     }
 
     private static String jsonSuccessResponse(String jsonStr) {
         String s = "Content-Type: application/json\n" +
                 "\r\n\r\n" + jsonStr;
-//
-        System.err.println(s);
-//
         return s;
     }
 
     private static String jsonErrorResponse(String message) {
         String s = "Content-Type: application/json\n" +
                 "\r\n\r\n" + "{\"error\":\"" + message + "\"}";
-//
-        System.err.println(s);
-//
         return s;
     }
 
@@ -164,15 +115,15 @@ public class Server {
         String s = "Content-Type: text/html\n" +
                 "\r\n\r\n" +
                 """
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Веб лаба 1</title>
-                </head>
-                <body>
-                    <h1 style="color: green">Результат выполнения</h1>
-                    <p>""" + message + """
+                        <!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Веб лаба 1</title>
+                        </head>
+                        <body>
+                            <h1 style="color: green">Результат выполнения</h1>
+                            <p>""" + message + """
                 </p>
                 </body>
                 </html>
@@ -184,15 +135,15 @@ public class Server {
         String s = "Content-Type: text/html\n" +
                 "\r\n\r\n" +
                 """
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Веб лаба 1</title>
-                </head>
-                <body>
-                    <h1 style="color: red">Ошибка выполнения</h1>
-                    <p>Описание:""" + message + """
+                        <!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Веб лаба 1</title>
+                        </head>
+                        <body>
+                            <h1 style="color: red">Ошибка выполнения</h1>
+                            <p>Описание:""" + message + """
                 </p>
                 </body>
                 </html>
@@ -208,5 +159,14 @@ public class Server {
             params.put(keyValue[0], keyValue[1]);
         }
         return params;
+    }
+
+    public static void addToLog(String message) {
+        try (PrintWriter logger = new PrintWriter(new FileWriter("/home/studs/s467204/httpd-root/fcgi-bin/log.txt", StandardCharsets.UTF_8, true))) {
+            String time = new SimpleDateFormat("dd-MM-yyyy HH:mm").format(Calendar.getInstance().getTime());
+            logger.println("[ " + time + " ]" + message);
+        } catch (IOException e) {
+            System.out.println(jsonErrorResponse("Ошибка ввода-вывода!"));
+        }
     }
 }
