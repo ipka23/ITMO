f7ac34e707a493e01a52306dff558479dc448c01
ipka23 <ilyaipka@gmail.com>
2025-09-20 00:39:04 +0300

    dd

--- 2 курс/Веб-программирование/lab1/web-app/index.js
+++ 2 курс/Веб-программирование/lab1/web-app/index.js
@@ -3,7 +3,7 @@ let x
 let y
 let r
 const form = document.getElementById("form")
-
+loadTableFromLocalStorage()
 function radioClick(e) {
     x = +e.target.value
 }
@@ -14,7 +14,7 @@ for (let i = 0; i < form.x.length; i++) {
 
 function errorMessage(elementId, inputField, errorMessage) {
     var error = document.getElementById(elementId)
-        error.innerHTML = "<span style='color: red; animation: 3s fadeOut ease-in forwards'>" +
+    error.innerHTML = "<span style='color: red; animation: 3s fadeOut ease-in forwards'>" +
         `${errorMessage}</span>`
     if (errorMessage !== undefined) {
         document.getElementById(inputField).value = ""
@@ -24,7 +24,7 @@ function errorMessage(elementId, inputField, errorMessage) {
 submitButton.onclick = function (e) {
     e.preventDefault()
     if (x === undefined) {
-        errorMessage("xError", "Выберите X!")
+        errorMessage("xError", "Р’С‹Р±РµСЂРёС‚Рµ X!")
     }
 
     y = document.getElementById("inputY").value;
@@ -32,16 +32,16 @@ submitButton.onclick = function (e) {
 
     const regexp = /^[-+]?[0-9]*[.,][0-9]+$|^[-+]?[0-9]+$/
     if (!regexp.test(y) || y === undefined) {
-        errorMessage("yError", "inputY","Введите Y в правильном формате!")
+        errorMessage("yError", "inputY","Р’РІРµРґРёС‚Рµ Y РІ РїСЂР°РІРёР»СЊРЅРѕРј С„РѕСЂРјР°С‚Рµ!")
     }
     if (!regexp.test(r) || r === undefined) {
-        errorMessage("rError", "inputR", "Введите R в правильном формате!")
+        errorMessage("rError", "inputR", "Р’РІРµРґРёС‚Рµ R РІ РїСЂР°РІРёР»СЊРЅРѕРј С„РѕСЂРјР°С‚Рµ!")
     }
     if (!(-3 <= y && y <= 3)) {
-        errorMessage("yError",  "inputY", "Введите значение Y в пределах [-3;3]!")
+        errorMessage("yError",  "inputY", "Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ Y РІ РїСЂРµРґРµР»Р°С… [-3;3]!")
     }
     if (!(2 <= r && r <= 5)) {
-        errorMessage("rError", "inputR", "Введите значение R в пределах [2;5]!")
+        errorMessage("rError", "inputR", "Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ R РІ РїСЂРµРґРµР»Р°С… [2;5]!")
     }
     else {
         hit(x, y, r)
@@ -51,59 +51,94 @@ submitButton.onclick = function (e) {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: `x=${x}&y=${y}&r=${r}`
-        }).then(response => response.json()).then(data => {
-            console.log(data)
-            if (data.error == null) {
-                updateTable(parseServerData(data))
+        }).then(response => response.json()).then(json => {
+            console.log(json)
+            if (json.error == null) {
+                updateTable(jsonToDict(json))
             } else {
                 // console.log(data.error)
             }
         }).catch(error => {
-            alert("Ошибка сервера! " + error.toString())
+            alert("РћС€РёР±РєР° СЃРµСЂРІРµСЂР°! " + error.toString())
         })
 
     }
+}
+function hit(x, y, r) {
+    const svg = document.getElementById("svg")
+    const rPxSize = svg.clientWidth / 3
+    const svgCenterX = 150
+    const svgCenterY = 150
+    const scale = rPxSize / r
+    const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+    dot.setAttributeNS(null, "r", "1%")
+    dot.setAttributeNS(null, "cx", (svgCenterX + x * scale).toString())
+    dot.setAttributeNS(null, "cy", (svgCenterY - y * scale).toString())
+    dot.setAttributeNS(null, "fill", "red")
+    dot.setAttributeNS(null, "visibility", "visible")
+    svg.appendChild(dot)
+}
 
-    function hit(x, y, r) {
-        const svg = document.getElementById("svg")
-        const rPxSize = svg.clientWidth / 3
-        const svgCenterX = 150
-        const svgCenterY = 150
-        const scale = rPxSize / r
-        const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
-        dot.setAttributeNS(null, "r", "1%")
-        dot.setAttributeNS(null, "cx", (svgCenterX + x * scale).toString())
-        dot.setAttributeNS(null, "cy", (svgCenterY - y * scale).toString())
-        dot.setAttributeNS(null, "fill", "red")
-        dot.setAttributeNS(null, "visibility", "visible")
-        svg.appendChild(dot)
+
+function updateTable(dict = {}) {
+    const table = document.getElementById("statsTable")
+    const row = table.insertRow()
+    const xCell = row.insertCell(0)
+    const yCell = row.insertCell(1)
+    const rCell = row.insertCell(2)
+    const statusCell = row.insertCell(3)
+    const dateCell = row.insertCell(4)
+    const executionTimeCell = row.insertCell(5)
+    xCell.textContent = dict["x"]
+    yCell.textContent = dict["y"]
+    rCell.textContent = dict["r"]
+    statusCell.textContent = dict["status"]
+    dateCell.textContent = dict["currentTime"]
+    executionTimeCell.textContent = dict["executionTime"]
+    updateLocalStorage(localStorage.length + 1, dictToString(dict))
+    console.log(`localStorage.length + 1: ${localStorage.length + 1}, stringItem ${dictToString(dict)}`)
+}
+function updateLocalStorage(index, dict){
+    localStorage.setItem(index.toString(), dictToString(dict))
+}
+function dictToString(dict) {
+    return `x: ${dict["x"]},
+            y: ${dict["y"]}
+            r: ${dict["r"]}
+            status: ${dict["status"]}
+            currentTime: ${dict["currentTime"]}
+            executionTime: ${dict["executionTime"]}
+            `
+}
+function stringToDict(string) {
+    let statsDict = string.split(", ")
+    let dictItem = []
+    for (let pair in statsDict) {
+        let keyVal = pair.split(": ")
+        dictItem[keyVal[0]] = keyVal[1]
+
     }
+    console.log(`parsedDictItem: ${dictItem}`)
+    return dictItem
+}
 
-    function parseServerData(json) {
-        let dict = {}
-        dict["x"] = json.x
-        dict["y"] = json.y
-        dict["r"] = json.r
-        dict["status"] = json.status
-        dict["currentTime"] = json.currentTime
-        dict["executionTime"] = json.executionTime
-        return dict
-    }
+function jsonToDict(json) {
+    let dict = {}
+    dict["x"] = json.x
+    dict["y"] = json.y
+    dict["r"] = json.r
+    dict["status"] = json.status
+    dict["currentTime"] = json.currentTime
+    dict["executionTime"] = json.executionTime
+    return dict
+}
 
-    function updateTable(dict = {}) {
-        const table = document.getElementById("statsTable")
-        const row = table.insertRow()
-        const xCell = row.insertCell(0)
-        const yCell = row.insertCell(1)
-        const rCell = row.insertCell(2)
-        const statusCell = row.insertCell(3)
-        const dateCell = row.insertCell(4)
-        const executionTimeCell = row.insertCell(5)
-        xCell.textContent = dict["x"]
-        yCell.textContent = dict["y"]
-        rCell.textContent = dict["r"]
-        statusCell.textContent = dict["status"]
-        dateCell.textContent = dict["currentTime"]
-        executionTimeCell.textContent = dict["executionTime"]
+function loadTableFromLocalStorage(){
+    let localStorageTableLinesCount = window.localStorage.length
+    for (let i = 0; i < localStorageTableLinesCount; i++) {
+        let item = window.localStorage.getItem(i.toString())
+        let dictItem = stringToDict(item)
+        updateTable(dictItem)
+        console.log(`localStorageLineIndex: ${i} dictItem: ${dictItem}`)
     }
 }
