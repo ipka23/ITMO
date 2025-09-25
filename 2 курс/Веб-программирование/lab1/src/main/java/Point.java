0fb027901734597aeb179577fffae333345ba95e
ipka23 <ilyaipka@gmail.com>
2025-09-26 01:19:07 +0300

    lab1 done

---
+++ 2 курс/Веб-программирование/lab1/src/main/java/Point.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+
+@AllArgsConstructor
+@ToString
+public class Point {
+    private String x;
+    private String y;
+    private String r;
+    private String hit;
+    private String currentTime;
+    private String executionTime;
+}
