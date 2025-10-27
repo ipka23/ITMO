07ed8703d5be99a19501702e2de9c625878aa3a4
Илья Пчелкин <ipka23@niuitmo.ru>
2025-10-27 23:59:50 +0300

    dd

--- 2 year/Prog langs/2 seminar - 30 sept/main.c
+++ 2 year/Prog langs/2 seminar - 30 sept/main.c
@@ -1,55 +1,75 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdint.h>
+#include <string.h>
 
+bool startsWith(const char *pre, const char *str) {
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
 
-// void addBook(arrayList* collection) {
-//
-// }
-//
-Book* getBookByISBN(arrayList* collection,  char* isbnCheck) {
+Book*   getBookByISBN(arrayList* collection,  char* isbnCheck) {
+    if (isbnCheck == NULL) return NULL;
+
     for (size_t i = 0; i < collection->size; i++) {
         Book* b = collection->items[i];
-        if (b->isbn == isbnCheck) {
+        if (b->isbn[0] == isbnCheck[0]) {
+            return b;
+        }
+        if (strcmp(b->isbn, isbnCheck) == 0) {
             return b;
         }
     }
     return NULL;
 }
-//
-// void searchBook();
+
+arrayList* searchBooksByKeyWord(arrayList* collection, char* keyWord) {
+    arrayList* matchList = newList(compareBooks, bookDestructor);
+    for (size_t i = 0; i < collection->size; i++) {
+        Book* b = collection->items[i];
+        bool match = startsWith(keyWord, b->title) || startsWith(keyWord, b->author) || startsWith(keyWord, b->annot) || startsWith(keyWord, b->isbn) || startsWith(keyWord, b->date);
+        if (match) {
+            addToList(matchList, b);
+        }
+    }
+    return matchList;
+}
 
 void runLibraryCatalog() {
     arrayList* bookCollection = newList(compareBooks, bookDestructor);
     for (;;) {
+        setbuf(stdout, 0);
         printf("Enter the number of action and press [Enter]. Then follow instructions.\nMenu:\n1 Add book\n2 Get book by ISBN\n3 Search books\n4 Exit\n> ");
         int8_t input;
         scanf("%d", &input);
+        // printf("input: %d\n", input); //
         if (input == 1) {
-            char* title;
-            char* author;
-            char* annot;
-            char* isbn;
-            char* date;
+            char title[20];
+            char author[20];
+            char annot[20];
+            char isbn[20];
+            char date[20];
 
             printf("New book\nTitle: ");
-            scanf("%s", &title);
+            scanf("%s", title);
             printf("Author: ");
-            scanf("%s", &author);
+            scanf("%s", author);
             printf("Annotation: ");
-            scanf("%s", &annot);
+            scanf("%s", annot);
             printf("ISBN: ");
-            scanf("%s", &isbn);
+            scanf("%s", isbn);
             printf("Publication date: ");
-            scanf("%s", &date);
+            scanf("%s", date);
 
-            Book b = {title, author, annot, isbn, date};
-            addToList(bookCollection, &b);
+            // printf("isbn: %s\n", isbn); //
+            Book* b = newBook(strdup(title), strdup(author), strdup(annot), strdup(isbn), strdup(date));
+            addToList(bookCollection, b);
 
         } else if (input == 2) {
-            char* isbn;
+            char isbn[13];
             printf("Enter ISBN: ");
-            scanf("%s", &isbn);
+            scanf("%s", isbn);
+            // printf("Entered isbn: %s\n", &isbn); //
+
             Book* book = getBookByISBN(bookCollection, isbn);
             if (book != NULL) {
                 printf("Book information:\n");
@@ -57,16 +77,36 @@ void runLibraryCatalog() {
                 printf("Author: %s\n", book->author);
                 printf("Annotation: %s\n", book->annot);
                 printf("ISBN: %s\n", book->isbn);
-                printf("Publication date: %s\n", book->date);
+                printf("Publication date: %s\n\n", book->date);
             } else {
-                printf("No book found with ISBN: %s\n", isbn);
+                printf("No book found with ISBN: %s\n\n", isbn);
             }
         } else if (input == 3) {
+            char request[20];
+            printf("Request: ");
+            scanf("%s", request);
+            printf("Searching...\n");
+            arrayList* matches = searchBooksByKeyWord(bookCollection, request);
+            printf("Results (%zu) :\n", matches->size);
 
+            if (matches->size == 0) {
+                printf("No results for request: %s!", request);
+                continue;
+            }
+            for (size_t i = 0; i < matches->size; i++) {
+                printf("#%zu    ", i + 1);
+                Book* b = matches->items[i];
+                printf("Book information:\n");
+                printf("Title: %s\n", b->title);
+                printf("Author: %s\n", b->author);
+                printf("Annotation: %s\n", b->annot);
+                printf("ISBN: %s\n", b->isbn);
+                printf("Publication date: %s\n\n", b->date);
+            }
         } else if (input == 4) {
             break;
         } else {
-            printf("Invalid input!\n");
+            printf("Invalid input!\n\n");
         }
     }
 }
