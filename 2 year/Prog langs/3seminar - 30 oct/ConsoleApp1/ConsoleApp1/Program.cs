b1078639918dcde88aea4307ae54407b1002d9ff
ipka23 <ilyaipka@gmail.com>
2025-10-28 21:08:50 +0300

    dd

---
+++ 2 year/Prog langs/3seminar - 30 oct/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        uint x = 2;
+        Console.WriteLine("dd");
+        Console.Write("dd");
+    }
+}
