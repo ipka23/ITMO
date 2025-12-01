c5ca6c74c66e1ca4a9c228a0becb4d4c613b77e0
ipka23 <ilyaipka@gmail.com>
2025-12-01 20:03:16 +0300

    dd

--- 2 курс/Веб-программирование/lab3/lab3/deploy.sh
+++ 2 курс/Веб-программирование/lab3/lab3/deploy.sh
@@ -1,4 +1,4 @@
 gradle build
 cd /Users/ipka23/Documents/wildfly-37.0.1.Final/bin
 ./jboss-cli.sh --connect --controller=localhost:25232
-deploy "/Users/ipka23/Desktop/ITMO/2 курс/Веб-программирование/lab2/lab2/build/libs/lab2.war" --force
+deploy "/Users/ipka23/Desktop/ITMO/2 курс/Веб-программирование/lab3/lab3/build/libs/lab3.war" --force
