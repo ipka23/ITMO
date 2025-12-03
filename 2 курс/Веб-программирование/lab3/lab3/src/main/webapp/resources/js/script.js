293803deafac9f5b4523ad4b904941082614531f
Илья Пчелкин <ipka23@niuitmo.ru>
2025-12-03 17:18:53 +0300

    lab 3 v01 done

--- 2 курс/Веб-программирование/lab3/lab3/src/main/webapp/resources/js/script.js
+++ 2 курс/Веб-программирование/lab3/lab3/src/main/webapp/resources/js/script.js
@@ -19,8 +19,7 @@ let previousR
 document.addEventListener("DOMContentLoaded", function () {
     form = document.getElementById("mainForm")
 
-    submitButton = document.getElementById("mainForm:submitButton")
-    submitButton.addEventListener("click", sendPointToServer)
+
 
     rInput = document.getElementById("mainForm:inputR")
     rInput.addEventListener("change", handleChangeR)
@@ -50,30 +49,10 @@ document.addEventListener("DOMContentLoaded", function () {
             r = +e.target.value
         })
     }
+    submitButton = document.getElementById("mainForm:submitButton")
+    submitButton.addEventListener("click", drawPoint)
 })
 
-
-// function getSelectedR() {
-//     const radioButtons = document.getElementsByName('mainForm:inputR');
-//
-//     for (const radio of radioButtons) {
-//         if (radio.checked) {
-//             return radio.value;
-//         }
-//     }
-//
-//     return undefined;
-// }
-
-// function handleChangeX() {
-//     if (xInput.value < -3 || xInput.value > 3) {
-//         if (xInput.value < 10) {
-//             xInput.value = ""
-//         } else {
-//             xInput.value = xInput.value[0]
-//         }
-//     }
-// }
 
 function handleInputY() {
     if (yInput.value < -3 || yInput.value > 3) {
@@ -95,73 +74,6 @@ function handleChangeR() {
     previousR = newR
 }
 
-function makeFetch(method, body, contentType) {
-    let currentLang = localStorage.getItem("lang")
-    if (method === "POST") {
-        alert("Ошибка: Введите GET запрос!")
-    } else if (method === "GET") {
-        if (body.drawByClick === false) {
-            fetch(`http://localhost:25230/lab2/controller?x=${body.x}&y=${body.y}&r=${body.r}&lang=${currentLang}`, {
-                method: "GET",
-            }).then(response => {
-                if (!response.ok) {
-                    throw new Error(`${response.status}`)
-                } else {
-                    return response.text()
-                }
-            }).then(resultHtml => {
-                document.open()
-                document.write(resultHtml)
-                document.close()
-            }).catch(error => {
-                alert("Ошибка: " + error.toString())
-            })
-
-        } else {
-            fetch(`http://localhost:25230/lab2/controller?x=${body.x}&y=${body.y}&r=${body.r}&drawByClick=${body.drawByClick}&lang=${currentLang}`, {
-                method: "GET",
-            }).then(response => response.json())
-                .then(json => {
-                    if (json.status !== "error") {
-                        let point = jsonToDict(json)
-                        drawPoint(point.x, point.y, point.r)
-                        updateTable(point, true)
-                    } else {
-                        console.log(json)
-                    }
-                }).catch(error => {
-                alert("Ошибка: " + error.toString())
-            })
-        }
-    }
-
-}
-
-function errorMessage(elementId, inputField, message) {
-    let error = document.getElementById(elementId)
-    error.innerHTML = "<span style='color: red; animation: 3s fadeOut ease-in forwards'>" +
-        `${message}</span>`
-
-    if (message !== undefined && inputField !== undefined && (inputField !== "mainForm:inputX" || inputField !== "mainForm:inputR")) {
-        const inputEl = document.getElementById(inputField)
-        if (inputEl) inputEl.value = ""
-    }
-}
-
-function jsonToDict(json) {
-    let dict = {}
-    dict["x"] = json.result.x
-    dict["y"] = json.result.y
-    dict["r"] = json.result.r
-    dict["status"] = json.result.status
-    dict["currentTime"] = json.result.currentTime
-    dict["executionTime"] = json.result.executionTime
-    return dict
-}
-
-function dictToJson(dict) {
-    return JSON.stringify(dict)
-}
 
 function svgToMathCoords(svgX, svgY, r) {
     const scale = rPxSize / r
@@ -183,45 +95,6 @@ function changeRadiusOnAxis(oldR, newR) {
     document.getElementById("rx/2").textContent = `${r / 2}`
     document.getElementById("ry/2").textContent = `${r / 2}`
     changePointR(oldR, newR)
-}
-
-function updateTable(dict, firstAdd) {
-    const table = document.getElementById("mainForm:statsTable")
-    const row = table.insertRow()
-    const xCell = row.insertCell(0)
-    const yCell = row.insertCell(1)
-    const rCell = row.insertCell(2)
-    const statusCell = row.insertCell(3)
-    const dateCell = row.insertCell(4)
-    const executionTimeCell = row.insertCell(5)
-    xCell.style.maxWidth = '15px'
-    yCell.style.maxWidth = '15px'
-    rCell.style.maxWidth = '15px'
-
-    if (dict["x"].length > 5) {
-        yCell.title = dict["y"]
-        yCell.style.overflow = "hidden"
-        dict["x"] = dict["x"].substring(0, 5) + "..."
-    }
-    xCell.textContent = dict["x"]
-
-    if (dict["y"].length > 5) {
-        yCell.title = dict["y"]
-        yCell.style.overflow = "hidden"
-        dict["y"] = dict["y"].substring(0, 5) + "..."
-    }
-    yCell.textContent = dict["y"]
-
-    if (dict["r"].length > 5) {
-        rCell.title = dict["r"]
-        rCell.style.overflow = "hidden"
-        dict["r"] = dict["r"].substring(0, 5) + "..."
-    }
-    rCell.textContent = dict["r"]
-
-    statusCell.textContent = dict["status"]
-    dateCell.textContent = dict["currentTime"]
-    executionTimeCell.textContent = dict["executionTime"]
 }
 
 // Actions with points ===============================================
@@ -249,15 +122,6 @@ function changePointR(oldR, newR) {
     }
 }
 
-function loadPoints() {
-    let points = document.querySelectorAll("circle")
-    for (let i = 0; i < points.length; i++) {
-        let p = points[i]
-        if (r === undefined) {
-            resetPointColor(p)
-        }
-    }
-}
 
 function changePointColor(x, y, r, point) {
     x = new Decimal(x);
@@ -274,7 +138,8 @@ function changePointColor(x, y, r, point) {
 function resetPointColor(point) {
     point.setAttributeNS(null, "fill", "black")
 }
-function drawPoint(x, y, r) {
+function drawPoint() {
+    // y = yInput.value
     dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
     scale = rPxSize / r
     dot.setAttributeNS(null, "r", "1%")
@@ -301,7 +166,7 @@ function drawPointByClick(e) {
     const coords = svgToMathCoords(svgX, svgY, r)
 
     if (r === undefined || r === "") {
-        errorMessage("svgError", undefined, "Выберите радиус R!")
+        document.getElementById("svgError").innerHTML = '<span style=\'color: red; animation: 3s fadeOut ease-in forwards\'>Выберите R!</span>'
     }
     else {
         remotePointCommand([
@@ -309,58 +174,14 @@ function drawPointByClick(e) {
             {name: 'y', value: coords.y},
             {name: 'r', value: r}
         ])
-        drawPoint(coords.x, coords.y, r)
-        // updateTable(point, true)
-
+        x = coords.x
+        y = coords.y
+        drawPoint()
     }
 
 }
 
 
-function sendPointToServer(e) {
-    e.preventDefault()
-    const regexp = /^[-+]?[0-9]*[.,][0-9]+$|^[-+]?[0-9]+$/
-    // x = xInput.value;
-    // yValues = initYvalues()
-    y = yInput.value
-    // r = rInput ? rInput.value : undefined;
-    let successX = true
-    let successY = true
-    let successR = true
-
-    if (x === undefined || x === "") {
-        console.log(`x: ${x}`)
-        errorMessage("mainForm:xError", "inputX", "Выберите X!")
-        successX = false
-    }
-
-    if (y === undefined || y === "") {
-        errorMessage("mainForm:yError", "inputY", 'Введите Y!')
-        successY = false
-    }
-    if (r === undefined || r === "") {
-        console.log(`r: ${r}`)
-        errorMessage("mainForm:rError", "inputR", 'Выберите R!')
-        successR = false
-    }
-
-    if (!regexp.test(y) && successY) {
-        errorMessage("mainForm:xError", "inputX", 'Введите Y в правильном формате!')
-        successX = false
-    }
-
-    if (!(-3 <= y && y <= 3) && successY) {
-        errorMessage("mainForm:xError", "inputX", 'Введите значение Y в пределах [-3;3]!')
-        successX = false
-    }
-     else if (successX && successY && successR) {
-        drawPoint(x, y, r)
-
-
-        // ...
-        // makeFetch("GET", {x: x, y: y, r: r, drawByClick: false}, 'application/x-www-form-urlencoded')
-    }
-}
 function checkPointHit(x, y, r) {
     x = new Decimal(x);
     y = new Decimal(y);
