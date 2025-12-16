c71a8048b090ed8fc152ca7513f3800c8e9f2858
ipka23 <ilyaipka@gmail.com>
2025-12-16 20:48:46 +0300

    fixed db?

--- 2 курс/Веб-программирование/lab3/lab3/src/main/java/controllers/DBManager.java
+++ 2 курс/Веб-программирование/lab3/lab3/src/main/java/controllers/DBManager.java
@@ -1,46 +1,20 @@
 package controllers;
 
+import jakarta.ejb.Stateless;
 import jakarta.persistence.*;
 import entities.Point;
 
-import java.util.ArrayList;
 import java.util.List;
-
+@Stateless
 public class DBManager {
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
+    @PersistenceContext(unitName = "persistenceUnit")
+    private EntityManager em;
 
-
-    public EntityManager getEntityManager() {
-        return emf.createEntityManager();
-    }
-
-    public ArrayList<Point> getPoints() {
-        EntityManager em = emf.createEntityManager();
-        ArrayList<Point> points = new ArrayList<>();
-
-        try {
-            TypedQuery<Point> query = em.createQuery("SELECT p FROM Point p", Point.class);
-            List<Point> resultList = query.getResultList();
-            points = new ArrayList<>(resultList);
-
-        } catch (Exception e) {
-        } finally {
-            if (em.isOpen()) em.close();
-        }
-        return points;
+    public List<Point> getPoints() {
+        return em.createQuery("SELECT p FROM Point p", Point.class).getResultList();
     }
 
     public void addPoint(Point point) {
-        EntityManager em = getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            em.persist(point);
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) tx.rollback();
-        } finally {
-            em.close();
-        }
+        em.persist(point);
     }
 }
