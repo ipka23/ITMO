d93ca37917008c0a0a3a283ca45499e0657b2200
Илья Пчелкин <ipka23@niuitmo.ru>
2025-12-17 15:03:02 +0300

    added lab 4

---
+++ 2 курс/Веб-программирование/lab4/lab4/src/main/java/entities/Point.java
@@ -0,0 +1,48 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "points")
+public class Point {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "x", nullable = false)
+    private BigDecimal x;
+
+    @Column(name = "y", nullable = false)
+    private BigDecimal y;
+
+    @Column(name = "r", nullable = false)
+    private BigDecimal r;
+
+    @Column(name = "result", nullable = false)
+    private String status;
+
+    @Column(name = "date", nullable = false)
+    private String currentTime;
+
+    @Column(name = "exectime", nullable = false)
+    private String executionTime;
+
+    public Point(BigDecimal x, BigDecimal y, BigDecimal r,
+                 String status, String currentTime, String executionTime) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.status = status;
+        this.currentTime = currentTime;
+        this.executionTime = executionTime;
+    }
+}
