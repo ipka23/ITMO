d2af2c0f319ff49572a7e948e7ac62ec3da90189
Илья Пчелкин <ipka23@niuitmo.ru>
2025-12-25 17:22:43 +0300

    task 5 done

--- 2 курс/Языки программирования/task 5 TODO/proglangstask5/proglangstask5/obj/Debug/net9.0/proglangstask5.AssemblyInfo.cs
+++ 2 курс/Языки программирования/task 5 TODO/proglangstask5/proglangstask5/obj/Debug/net9.0/proglangstask5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proglangstask5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fdaa88419f2ec341e386ed62fedfa9f2a48c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b30cbd8fc66cf559852419f77cb8f1beb224c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("proglangstask5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proglangstask5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
