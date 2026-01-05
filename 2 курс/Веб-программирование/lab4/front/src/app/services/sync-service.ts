ddacd9bb78561a308b72dc99588944771e5abf07
ipka23 <ilyaipka@gmail.com>
2026-01-05 20:43:58 +0300

    dd

---
+++ 2 курс/Веб-программирование/lab4/front/src/app/services/sync-service.ts
@@ -0,0 +1,21 @@
+import {Injectable, OnInit} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SyncService implements OnInit{
+  constructor(private _tableLoaded: BehaviorSubject<boolean>) {
+  }
+
+  set tableLoaded(behaviorSubject: BehaviorSubject<boolean>) {
+    this._tableLoaded = behaviorSubject
+  }
+
+  ngOnInit() {
+    // this._tableLoaded.asObservable().subscribe(isReady => {
+    //
+    // })
+  }
+
+}
