e7a6099f6fa738600b73680d58bfd96a7eb14a8d
Илья Пчелкин <ipka23@niuitmo.ru>
2026-01-10 22:43:17 +0300

    before adding types

--- 2 курс/Веб-программирование/lab4/front/src/test.ts
+++ 2 курс/Веб-программирование/lab4/front/src/test.ts
@@ -5,9 +5,9 @@ export type PointA = {
   status: string;
   currentTime: string;
   executionTime: string;
+
 }
-
-
+// before adding types
 const a: PointA = {x: "1", y: "2", r: "3",  status: "ok", currentTime: "now", executionTime: "0.1s"};
 
 console.log(a.x, a.y);
