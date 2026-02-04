9fed042834694718b675066bd7cb568a871490fd
Илья Пчелкин <ipka23@niuitmo.ru>
2026-02-04 11:05:52 +0300

    dd

--- 2 курс/Языки программирования/task 3 TODO/proglangstask3/cmake-build-debug/CMakeFiles/proglangstask3.dir/DependInfo.cmake
+++ 2 курс/Языки программирования/task 3 TODO/proglangstask3/cmake-build-debug/CMakeFiles/proglangstask3.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/ipka23/ITMO/2 курс/Языки программирования/4 seminar - 11 nov/proglangstask3/main.c" "CMakeFiles/proglangstask3.dir/main.c.obj" "gcc" "CMakeFiles/proglangstask3.dir/main.c.obj.d"
+  "C:/Users/ipka23/ITMO/2 курс/Языки программирования/task 3 TODO/proglangstask3/main.c" "CMakeFiles/proglangstask3.dir/main.c.obj" "gcc" "CMakeFiles/proglangstask3.dir/main.c.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
