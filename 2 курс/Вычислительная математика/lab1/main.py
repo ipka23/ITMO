985a096c8ffefeb952f6ebaa6f21ed84e7d75096
ipka23 <ilyaipka@gmail.com>
2026-02-09 19:03:37 +0300

    simple iteration method done

--- 2 курс/Вычислительная математика/lab1/main.py
+++ 2 курс/Вычислительная математика/lab1/main.py
@@ -1,5 +1,7 @@
 from matrixUtil import MatrixUtil
+from printUtil import PrintUtil
 
-matrixUtil = MatrixUtil()
+printUtil = PrintUtil()
+matrixUtil = MatrixUtil(printUtil)
 if __name__ == "__main__":
     matrixUtil.getMatrixFromInput()
