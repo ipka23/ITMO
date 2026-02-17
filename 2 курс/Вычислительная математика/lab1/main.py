f470128265a717e7a415be89b71e209754957f93
ipka23 <ilyaipka@gmail.com>
2026-02-17 12:49:29 +0300

    lab1 compmath done; todo user input processing

--- 2 курс/Вычислительная математика/lab1/main.py
+++ 2 курс/Вычислительная математика/lab1/main.py
@@ -4,4 +4,15 @@ from printUtil import PrintUtil
 printUtil = PrintUtil()
 matrixUtil = MatrixUtil(printUtil)
 if __name__ == "__main__":
-    matrixUtil.getMatrixFromInput()
+    while True:
+        print("="*93)
+        print("Выберите откуда считать матрицу:\n1 - ввод в консоли\n2 - файл")
+        userInput = input()
+        if userInput.strip() == "1":
+            matrixUtil.getMatrixFromInput()
+        elif userInput == "2":
+            print("Введите название файла: ")
+            fileName = input()
+            matrixUtil.getMatrixFromFile(fileName)
+        else:
+            print("Введите корректный вариант")
