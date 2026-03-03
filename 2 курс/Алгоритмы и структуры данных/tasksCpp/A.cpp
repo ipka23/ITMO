f47234ddcd8455bcabbbc7f49e79914e7ee80b61
ipka23 <ilyaipka@gmail.com>
2026-03-03 09:46:39 +0300

    dd

--- 2 курс/Алгоритмы и структуры данных/tasksCpp/A.cpp
+++ 2 курс/Алгоритмы и структуры данных/tasksCpp/A.cpp
@@ -1,61 +1,54 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 
 void inputArray(std::vector<int>& arr, int n) {
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; i++) {
     int x;
-    std::cin >> x;
+    scanf("%d", &x);
     arr[i] = x;
   }
 }
+
 void sortFlowers(std::vector<int>& arr, int n) {
   int start = 0;
-  int count = 0;
-  int maxCount = 0;
-  int minStart = 200000;
-  int minEnd = 200000;
-  int repeat = 0;
-  for (int i = 0; i < arr.size() - 1; ++i) {
-    if (arr[i] == arr[i + 1]) {
-      if (repeat < 2) {
-        repeat++;
-        count++;
-      } else {
-        start = i;
-        repeat++;
-        count = 0;
-      }
+  int bestStart = 0;
+  int bestEnd = 0;
+  int maxLen = 1;
+  int sameCounter = 1;
+  for (int i = 1; i < n; i++) {
+    if (arr[i] == arr[i - 1]) {
+      sameCounter++;
     } else {
-      repeat = 1;
-      count++;
+      sameCounter = 1;
     }
-
-    if (i == arr.size() - 2) {
-      count++;
-    }
-    if (maxCount < count) {
-        maxCount = count;
-        minStart = start;
-        minEnd = minStart + maxCount;
-      } else if (maxCount == count) {
-        minStart = start;
-        minEnd = minStart + count;
+    if (sameCounter == 3) {
+      int currLen = i - start;
+      if (currLen > maxLen) {
+        maxLen = currLen;
+        bestStart = start;
+        bestEnd = i - 1;
       }
+      start = i - 1;
+      sameCounter = 2;
+    }
+    int currLen = i - start + 1;
+    if (currLen > maxLen) {
+      maxLen = currLen;
+      bestStart = start;
+      bestEnd = i;
+    }
   }
-  // int end = start + maxCount;
-  minEnd = minStart + maxCount;
-  // std::cout << start + 1 << " " << end + 1 << std::endl;
-  std::cout << minStart + 1 << " " << minEnd + 1 << std::endl;
+  int currLen = n - start;
+  if (currLen > maxLen) {
+    bestStart = start;
+    bestEnd = n - 1;
+  }
+  printf("%d %d", bestStart + 1, bestEnd + 1);
 }
 
 void inputFlowers() {
   int n;
-  while (true) {
-    std::cin >> n;
-    if (n > 0 && n < 200000) {
-      break;
-    }
-  }
+  scanf("%d", &n);
   std::vector<int> flowers(n);
   inputArray(flowers, n);
   sortFlowers(flowers, n);
