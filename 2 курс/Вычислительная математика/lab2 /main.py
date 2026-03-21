3421332e394833137e76925c13b2fd2297b18c1e
ipka23 <ilyaipka@gmail.com>
2026-03-21 16:39:20 +0300

    dd

--- 2 курс/Вычислительная математика/lab2 /main.py
+++ 2 курс/Вычислительная математика/lab2 /main.py
@@ -1,14 +1,9 @@
-# import numpy as np
+import numpy as np
 eps = 10 ** -2
 funct1 = [(-1.8, 3), (-2.94, 2), (10.37, 1), (5.38, 0)]
 funct2 = [(-1.8, 3), (-2.94, 2), (10.37, 1), (5.38, 0)]  # todo
 funct3 = [(-1.8, 3), (-2.94, 2), (10.37, 1), (5.38, 0)]  # todo
 n = 1
-
-
-def roundPrint(val, n):
-    print(round(val, n))
-
 
 # l - коэффициент умножения
 # add - слагаемое для прибавления к функции
@@ -17,7 +12,6 @@ def f(x, l=1, add=0):
     # x = round(x, 2)
     if n == 1:
         for tup in funct1:
-            print(f"l4 = {l}")
             # print(f"===\nl={l}, c0={c[0]}, x={x}, c1={c[1]}\n")
             res += l * tup[0] * x ** tup[1]
             # print(f"r = {l * c[0] * x ** c[1]}\n===")
@@ -27,10 +21,6 @@ def f(x, l=1, add=0):
     elif n == 3:
         for c in funct3:
             res += l * c[0] * x ** c[1]
-    # if sub == True:
-    #     return 1 - res
-
-    # else:
     return res + add
 
 
@@ -38,67 +28,90 @@ def calcDiff(x):
     return (f(x + eps) - f(x)) / eps
 
 
-def calcMaxOfDiffFunction(a, b):
-    m = -1
-    floats = [round(a + i * eps, 2) for i in range(int((b - a) / eps) + 1)]
-    for x_0 in floats:
-        m = max(m, calcDiff(x_0))
-    return m
-
-
-def halfDivision(a, b):
-    x = (a + b) / 2
-    # print(f"abs f(x) = {abs(f(x))}")
-    # print(f"x = {x}")
-    if abs(f(x)) <= eps:
-        # print(f"return x = {x}")
-        return x
-    if f(a) * f(x) > 0:
-        return halfDivision(x, b)
-    else:
-        return halfDivision(a, x)
-
-
 def fi(x, l):
-    print(f"l3 = {l}")
     return f(x, l, x)
 
 
-def simpleIteration(a, b, x):
-    l = -1 / calcMaxOfDiffFunction(a, b)
-    print(f"l1 = {l}")
-    if abs(f(x)) <= eps:
-        return x
-    else:
-        print(f"l2 = {l}")
-        return simpleIteration(a, b, fi(x, l=l))
+def simpleIteration(a, b):
+    x_curr = a
+    l = -1 / max(calcDiff(a), calcDiff(b))
+    n = 1
+    while True:
+        x_next = fi(x_curr, l)
+        if abs(x_next - x_curr) <= eps:
+            break
+        if (abs(x_next - x_curr)) > 10 or n > 20:
+            print("Введите более точные границы, с текущими последовательность решений не сходится к истинному")
+            break
+
+        # print(f"n = {n} | x_{n} = {x_curr:.3f} | x_{n+1} = {x_next:.3f} | f(x_{n+1}) = {f(x_next):.3f} | |x_next - x_curr| = {abs(x_next - x_curr):.3f}")
+        x_curr = x_next
+        n += 1
+    print(round(x_next, 3))
 
 
-def secants(a, b):
-    pass
+def tangents(a, b):
+    x_mid = (a + b) / 2
+    x_prev = x_mid
+    if calcDiff(x_mid) > 0:
+        x_prev = a
+    elif calcDiff(x_mid) < 0:
+        x_prev = b
+    while True:
+        h_prev = f(x_prev) / calcDiff(x_prev)
+        x_curr = x_prev - h_prev
+        if abs(x_curr - x_prev) <= eps:
+            break
+        x_prev = x_curr
+    print(x_curr)
+
+
+def chords(a, b):
+    while True:
+        x_curr = a - f(a) * (a - b) / (f(a) - f(b))
+        if abs(f(x_curr)) <= eps:
+            break
+        # f_i = f(x_i)
+        if f(a) * f(x_curr) < 0:
+            b = x_curr
+        else:
+            a = x_curr
+    print(x_curr)
 
 
 def solveEquation():
     # −1,8𝑥^3 − 2,94𝑥^2 + 10,37𝑥 + 5,38 = [(-1.8, 3), (−2.94, 2), (10.37, 1), (5.38, 0)]
     a1 = -4
-    b1 = -2
+    b1 = -3
 
     a2 = -1
     b2 = 0
 
-    a3 = 1
-    b3 = 3
-    # x3_0 = (a3 + b3) / 2
-    # x_right = simpleIteration(a3, b3, x3_0)
-    x_right = halfDivision(a3, b3)  # Крайний правый корень
-    print(x_right)
-    x1_0 = (a1 + b1) / 2
-    x_left = simpleIteration(a1, b1, x1_0)  # Крайний левый корень
-    print(x_left)
-    x2_0 = (a2 + b2) / 2
-    # x_mid = simpleIteration(a2, b2, x2_0)  # Центральный корень
-    # print(x_mid)
+    a3 = 1.5
+    b3 = 2
+
+    # simpleIteration(a1, b1)
+    # simpleIteration(a2, b2)
+    # print("-" * 100)
+    # simpleIteration(a3, b3)
+
+    # chords(a1, b1)
+    # print("-" * 100)
+    # chords(a2, b2)
+    # print("-" * 100)
+    # chords(a3, b3)
+
+    tangents(a1, b1)
+    print("-" * 100)
+    tangents(a2, b2)
+    print("-" * 100)
+    tangents(a3, b3)
+
+
+def solveSystemOfEquation():
+    pass
 
 
 if __name__ == "__main__":
-    solveEquation()
+    # solveEquation()
+    solveSystemOfEquation()
